29d51707e0d51387e0f24c2a809e234b0cf97dcc
lichen <lichen@daojia.com>
2018-07-24 12:55:29 +0800

    add new method

---
+++ src/main/java/ink/lichen/leecode/TreeNode.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode;
+
+/**
+ * Created by lichen@daojia.com on 2018-7-17.
+ */
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+
+}
