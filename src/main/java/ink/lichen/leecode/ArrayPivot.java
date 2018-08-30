6a68fa9a8fd10d1cd0617f9d24eafb12d2cfe0a5
lichen <lichen@daojia.com>
2018-08-30 17:31:57 +0800

    add arrayPivot

---
+++ src/main/java/ink/lichen/leecode/ArrayPivot.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode;
+
+/**
+ * Created by lichen@daojia.com on 2018-8-30.
+ */
+public class ArrayPivot {
+
+    public int pivotIndex(int[] nums) {
+
+        return 0;
+    }
+
+}
