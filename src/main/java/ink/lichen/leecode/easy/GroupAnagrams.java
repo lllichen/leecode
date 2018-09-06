f2c7af8a7652481b95b60b8fc0ef936d8d637c2c
lichen <lichen@daojia-inc.com>
2018-09-06 18:10:20 +0800

    add groupAnagrams

---
+++ src/main/java/ink/lichen/leecode/easy/GroupAnagrams.java
@@ -0,0 +1,7 @@
+package ink.lichen.leecode.easy;
+
+/**
+ * Created by lichen@daojia.com on 2018-9-6.
+ */
+public class GroupAnagrams {
+}
--- src/main/java/ink/lichen/leecode/easy/SetZero.java
+++ src/main/java/ink/lichen/leecode/easy/SetZero.java
@@ -6,13 +6,32 @@ package ink.lichen.leecode.easy;
 public class SetZero {
 
     public void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
         for (int i = 0 ; i < matrix.length; i++){
             for (int j = 0 ; j < matrix[i].length; j ++){
                 if (matrix[i][j] == 0){
-
+                    for (int temp = 0 ; temp < n ; temp ++){
+                        matrix[i][temp] = Integer.MIN_VALUE;
+                    }
+                    for (int temp = 0 ; temp < m ; temp++){
+                        matrix[temp][j] = Integer.MIN_VALUE;
+                    }
                 }
             }
         }
+
+        for (int i = 0 ; i < matrix.length; i++){
+            for (int j = 0 ; j < matrix[i].length; j ++){
+                if (matrix[i][j] == Integer.MIN_VALUE){
+                    matrix[i][j]  = 0;
+                }
+            }
+        }
+
+
     }
 
     public static void main(String[] args) {
