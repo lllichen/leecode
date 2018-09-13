2590d7ceb1004806565cb07f5fb9704746cd82e6
lichen <lichen@daojia-inc.com>
2018-09-13 14:55:06 +0800

    add increasingTriple

--- src/main/java/ink/lichen/leecode/medium/IncreasingTriplet.java
+++ src/main/java/ink/lichen/leecode/medium/IncreasingTriplet.java
@@ -7,17 +7,22 @@ public class IncreasingTriplet {
 
     public boolean increasingTriplet(int[] nums) {
 
-        int m1 =Integer.MAX_VALUE;
-        int m2 = Integer.MAX_VALUE;
-
-        for (int a : nums)
-        {
-            if (m1 >= a){
-                m1 =a;
-            }
-            else if (m2 >= a) m2 = a;
-            else return true;
+        if (nums.length< 3){
+            return false;
         }
-        return false;
+
+       int first = Integer.MAX_VALUE;
+       int second = Integer.MAX_VALUE;
+
+       for (int num : nums){
+           if (first> num){
+               first = num;
+           }else if ( num > first && num < second ){
+               second = num;
+           }else if (num > second){
+               return true;
+           }
+       }
+       return false;
     }
 }
