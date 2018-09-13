36d78676511f454794d03b8e0215aedd40dde380
lichen <lichen@daojia-inc.com>
2018-09-13 14:57:40 +0800

    add oddEvenList

---
+++ src/main/java/ink/lichen/leecode/medium/OddEvenList.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode.medium;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-9-13.
+ */
+public class OddEvenList {
+    public ListNode oddEvenList(ListNode head) {
+
+        return null;
+    }
+}
