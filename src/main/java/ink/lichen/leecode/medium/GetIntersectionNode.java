c42a2d391d3c3adb38917538385743f315c4128c
lichen <lichen@daojia-inc.com>
2018-09-14 17:33:32 +0800

    fix error

--- src/main/java/ink/lichen/leecode/medium/GetIntersectionNode.java
+++ src/main/java/ink/lichen/leecode/medium/GetIntersectionNode.java
@@ -20,16 +20,45 @@ import java.util.Set;
 public class GetIntersectionNode {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> set = new HashSet<>();
-        while (headA != null){
-            set.add(headA);
-            headA = headA.next;
+//        Set<ListNode> set = new HashSet<>();
+//        while (headA != null){
+//            set.add(headA);
+//            headA = headA.next;
+//        }
+//        while (headB != null){
+//            if (set.contains(headB)){
+//                return headB;
+//            }else{
+//                headB = headB.next;
+//            }
+//        }
+        if(headA == null || headB == null){
+            return null;
         }
-        while (headB != null){
-            if (set.contains(headB)){
-                return headB;
-            }else{
+        ListNode dummyA = headA;
+        ListNode dummyB = headB;
+
+        while (headA != null || headB != null || dummyA != null || dummyB != null ){
+            ListNode temp;
+            ListNode tempb;
+            if (headA != null){
+                temp = headA;
+                headA = headA.next;
+            }else {
+                temp = headB;
                 headB = headB.next;
+            }
+
+            if (dummyB != null){
+                tempb = dummyB;
+                dummyB = dummyB.next;
+            }else {
+                tempb = dummyA;
+                dummyA = dummyA.next;
+            }
+
+            if(temp == tempb){
+                return temp;
             }
         }
         return null;
