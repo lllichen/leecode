992db683fa0dadfbad1e77225154343935e55dbc
lichen <lichen@daojia-inc.com>
2018-09-14 16:34:42 +0800

    add listNode

--- src/main/java/ink/lichen/leecode/medium/GetIntersectionNode.java
+++ src/main/java/ink/lichen/leecode/medium/GetIntersectionNode.java
@@ -1,7 +1,18 @@
 package ink.lichen.leecode.medium;
 
+import ink.lichen.leecode.support.ListNode;
+
 /**
  * Created by lichen@daojia.com on 2018-9-14.
  */
 public class GetIntersectionNode {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+
+
+//        while(head.next != null)
+        return null;
+    }
+
 }
