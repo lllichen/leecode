c4a396456f2051a0cd8faefe551fca79a10d75a2
lichen <lichen@daojia-inc.com>
2018-09-14 16:45:36 +0800

    add code

--- src/main/java/ink/lichen/leecode/medium/GetIntersectionNode.java
+++ src/main/java/ink/lichen/leecode/medium/GetIntersectionNode.java
@@ -2,16 +2,36 @@ package ink.lichen.leecode.medium;
 
 import ink.lichen.leecode.support.ListNode;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by lichen@daojia.com on 2018-9-14.
+ *
+ *    A:          a1 → a2
+ *                       ↘
+                         c1 → c2 → c3
+                         ↗
+      B:     b1 → b2 → b3
+ *
+ * time o(n)  space o(1)
  */
 public class GetIntersectionNode {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-
-
-
-//        while(head.next != null)
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null){
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null){
+            if (set.contains(headB)){
+                return headB;
+            }else{
+                headB = headB.next;
+            }
+        }
         return null;
     }
 
