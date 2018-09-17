5fbf2597421d94f97606fe188374217e985ac53a
lichen <lichen@daojia-inc.com>
2018-09-17 12:30:09 +0800

    2018-9-17 number one

--- src/main/java/ink/lichen/leecode/medium/InorderTraversal.java
+++ src/main/java/ink/lichen/leecode/medium/InorderTraversal.java
@@ -1,17 +1,39 @@
 package ink.lichen.leecode.medium;
 
+import ink.lichen.leecode.support.ListNode;
 import ink.lichen.leecode.support.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by lichen@daojia.com on 2018-9-14.
+ *  [1,null,2,3]
+ *      1
+         \
+          2
+         /
+        3
+ *  [1,3,2]
  */
 public class InorderTraversal {
 
     public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
 
-        return null;
+        if (root.left != null)
+        {
+            list.addAll(inorderTraversal(root.left));
+        }
+
+        list.add(root.val);
+
+        if (root.right != null)
+        {
+            list.addAll(inorderTraversal(root.right));
+        }
+
+        return list;
     }
 
 }
