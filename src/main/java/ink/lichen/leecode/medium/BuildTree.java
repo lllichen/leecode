f27a318d23e3e08456f4d693be280c0c07ea7b95
lichen <lichen@daojia-inc.com>
2018-09-17 16:42:25 +0800

    build tree

---
+++ src/main/java/ink/lichen/leecode/medium/BuildTree.java
@@ -0,0 +1,15 @@
+package ink.lichen.leecode.medium;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-9-17.
+ */
+public class BuildTree {
+
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        return null;
+    }
+}
