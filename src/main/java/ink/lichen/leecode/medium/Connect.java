96d2339a676411d5b2b0de51ff3bc61ed2434be8
lichen <lichen@daojia-inc.com>
2018-09-18 14:57:50 +0800

    add TreeLinkNode

---
+++ src/main/java/ink/lichen/leecode/medium/Connect.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.medium;
+
+import ink.lichen.leecode.support.TreeLinkNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-9-18.
+ */
+public class Connect {
+
+
+    public void connect(TreeLinkNode root) {
+
+    }
+}
---
+++ src/main/java/ink/lichen/leecode/support/TreeLinkNode.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.support;
+
+/**
+ * Created by lichen@daojia.com on 2018-9-18.
+ */
+public class TreeLinkNode {
+
+      public int val;
+      public TreeLinkNode left, right, next;
+      public TreeLinkNode(int x) { val = x; }
+
+}
