36e9c11ffd3cf3b3d95e6c1b7970c7c33267d95c
lichen <lichen@daojia-inc.com>
2018-09-18 17:47:28 +0800

    learn dsf algothm

--- src/main/java/ink/lichen/leecode/medium/NumIslands.java
+++ src/main/java/ink/lichen/leecode/medium/NumIslands.java
@@ -24,9 +24,26 @@ package ink.lichen.leecode.medium;
  *
  */
 public class NumIslands {
-
     public int numIslands(char[][] grid) {
+        int res = 0;
+        if(grid.length==0) return res;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j]=='1'){
+                    searchIsland(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
 
-        return 0;
+    private void searchIsland(char[][] grid, int i, int j){
+        grid[i][j]='0';
+        // 搜索该点连通的上下左右
+        if(i>0 && grid[i-1][j]=='1') searchIsland(grid, i-1, j);
+        if(j>0 && grid[i][j-1]=='1') searchIsland(grid, i, j-1);
+        if(i<grid.length-1 && grid[i+1][j]=='1') searchIsland(grid, i+1, j);
+        if(j<grid[0].length-1 && grid[i][j+1]=='1') searchIsland(grid, i, j+1);
     }
 }
