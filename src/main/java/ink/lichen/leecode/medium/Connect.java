c3087dec5cac19d7bc3eec6dc9dd53ca0fc9c083
lichen <lichen@daojia-inc.com>
2018-09-18 14:59:38 +0800

    add Connect

--- src/main/java/ink/lichen/leecode/medium/Connect.java
+++ src/main/java/ink/lichen/leecode/medium/Connect.java
@@ -4,6 +4,19 @@ import ink.lichen.leecode.support.TreeLinkNode;
 
 /**
  * Created by lichen@daojia.com on 2018-9-18.
+ *
+ *     1
+     /  \
+    2    3
+   / \  / \
+  4  5 6  7
+
+       1 -> NULL
+     /  \
+    2 -> 3 -> NULL
+   / \  / \
+  4->5->6->7 -> NULL
+ *
  */
 public class Connect {
 
