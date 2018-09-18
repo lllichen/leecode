f18a4d5876a6b513a966d790bc0325806ff2c5af
lichen <lichen@daojia-inc.com>
2018-09-18 17:37:41 +0800

    add comment

--- src/main/java/ink/lichen/leecode/medium/NumIslands.java
+++ src/main/java/ink/lichen/leecode/medium/NumIslands.java
@@ -2,6 +2,25 @@ package ink.lichen.leecode.medium;
 
 /**
  * Created by lichen@daojia.com on 2018-9-18.
+ *
+ * 输入:
+ 11110
+ 11010
+ 11000
+ 00000
+ 输出: 1
+
+ 输入:
+ 11000
+ 11000
+ 00100
+ 00011
+
+ 输出: 3
+
+
+ *
+ *
  */
 public class NumIslands {
 
