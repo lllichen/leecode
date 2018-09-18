c19006e4a321fd7181361d318db0744e7afce60f
lichen <lichen@daojia-inc.com>
2018-09-18 17:38:46 +0800

    add comment

--- src/main/java/ink/lichen/leecode/medium/NumIslands.java
+++ src/main/java/ink/lichen/leecode/medium/NumIslands.java
@@ -3,7 +3,10 @@ package ink.lichen.leecode.medium;
 /**
  * Created by lichen@daojia.com on 2018-9-18.
  *
- * 输入:
+ 给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，
+ 并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+ *
+ 输入:
  11110
  11010
  11000
@@ -17,8 +20,6 @@ package ink.lichen.leecode.medium;
  00011
 
  输出: 3
-
-
  *
  *
  */
