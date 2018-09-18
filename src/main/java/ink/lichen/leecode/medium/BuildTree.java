5245316312251ae5f86a9a9516c8f047ef70959c
lichen <lichen@daojia-inc.com>
2018-09-18 12:30:07 +0800

    add build tree

--- src/main/java/ink/lichen/leecode/medium/BuildTree.java
+++ src/main/java/ink/lichen/leecode/medium/BuildTree.java
@@ -6,15 +6,33 @@ import java.util.Calendar;
 
 /**
  * Created by lichen@daojia.com on 2018-9-17.
+ *
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ *
+ *    3
+     / \
+    9  20
+      /  \
+     15   7
+ *
  */
 public class BuildTree {
 
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0){
+            return null;
+        }
+        for (int i = 0 ; i < preorder.length; i++){
+            TreeNode root = new TreeNode(preorder[i]);
+            for (int j = 0 ; j < inorder.length; j++){
+
+            }
+        }
+
         return null;
     }
 
-    public static void main(String[] args) {
 
-    }
 }
