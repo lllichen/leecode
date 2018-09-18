95344561468119e9068c807a93a74c444e0841c2
lichen <lichen@daojia-inc.com>
2018-09-18 14:27:58 +0800

    build tree

--- src/main/java/ink/lichen/leecode/medium/BuildTree.java
+++ src/main/java/ink/lichen/leecode/medium/BuildTree.java
@@ -2,8 +2,6 @@ package ink.lichen.leecode.medium;
 
 import ink.lichen.leecode.support.TreeNode;
 
-import java.util.Calendar;
-
 /**
  * Created by lichen@daojia.com on 2018-9-17.
  *
@@ -25,24 +23,24 @@ public class BuildTree {
         int preStart = 0;
         int inStart = 0;
         int inEnd = preorder.length;
-
         return help(preStart,inStart,inEnd,preorder,inorder);
     }
 
     private TreeNode help(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder ){
         int index = 0;
         TreeNode root = new TreeNode(preorder[preStart]);
-        if ( preStart > index || inStart > inEnd ){
-            return null;
-        }
+//        if ( preStart > index || inStart > inEnd ){
+//            return null;
+//        }
 
         for ( int i = inStart; i < inEnd; i++ ){
             if (inorder[i] == root.val){
                 index = i;
+                break;
             }
         }
-        root.left = help(preStart,index,inEnd,preorder,inorder);
-        root.right = help(preStart,index,inEnd,preorder,inorder);
+        root.left = help(preStart+1,inStart,index,preorder,inorder);
+        root.right = help(preStart+index - inStart+1,index+1,inEnd,preorder,inorder);
         return root;
     }
 
