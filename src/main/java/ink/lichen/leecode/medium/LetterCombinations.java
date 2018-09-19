9a3dab1c3cd271f0da84fbfaef15a71d14ef9e83
lichen <lichen@daojia-inc.com>
2018-09-19 17:12:18 +0800

    add code

--- src/main/java/ink/lichen/leecode/medium/LetterCombinations.java
+++ src/main/java/ink/lichen/leecode/medium/LetterCombinations.java
@@ -12,43 +12,66 @@ import java.util.Set;
  */
 public class LetterCombinations {
 
+//    public List<String> letterCombinations(String digits) {
+//        Map<Character, String> map = new HashMap<>();
+//        map.put('2', "abc");
+//        map.put('3', "def");
+//        map.put('4', "ghi");
+//        map.put('5', "jkl");
+//        map.put('6', "mno");
+//        map.put('7', "pqrs");
+//        map.put('8', "tuv");
+//        map.put('9', "wxyz");
+//
+//        Set<String> result = new HashSet<>();
+//
+//        for (int i = 0; i < digits.length(); i++) {
+//            char c = digits.charAt(i);
+//            String val = map.get(c);
+//            if (result.size() == 0) {
+//                for (char t : val.toCharArray()) {
+//                    result.add(t + "");
+//                }
+//            } else {
+//                Set<String> temp = new HashSet<>();
+//                for (String sval : result) {
+//                    for(char z : val.toCharArray())
+//                    {
+//                        temp.add(sval+z);
+//                    }
+//                }
+//                result = temp;
+//            }
+//        }
+//
+//        return new ArrayList<>(result);
+//    }
+
+    String[] codes = new String[]{"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     public List<String> letterCombinations(String digits) {
-        Map<Character, String> map = new HashMap<>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+        if(digits == null || digits.equals("")){
+            return new ArrayList<>();
+        }
+        List<String> result = new ArrayList<>();
+        backtrace(digits, result, "", 1, digits.length());
+        return result;
+    }
 
-        Set<String> result = new HashSet<>();
-
-        for (int i = 0; i < digits.length(); i++) {
-            char c = digits.charAt(i);
-            String val = map.get(c);
-            if (result.size() == 0) {
-                for (char t : val.toCharArray()) {
-                    result.add(t + "");
-                }
-            } else {
-                Set<String> temp = new HashSet<>();
-                for (String sval : result) {
-                    for(char z : val.toCharArray())
-                    {
-                        temp.add(sval+z);
-                    }
-                }
-                result = temp;
+    private void backtrace(String digits, List<String> list, String str, int deep, int n){
+        if(deep == n){
+            for(char c : codes[digits.charAt(deep-1) - '0' -2].toCharArray()){
+                list.add(str+c);
+            }
+        }else{
+            for(char c : codes[digits.charAt(deep-1) - '0' -2].toCharArray()){
+                backtrace(digits, list, str+c, deep+1, n);
             }
         }
-
-        return new ArrayList<>(result);
     }
 
     public static void main(String[] args) {
         LetterCombinations letterCombinations = new LetterCombinations();
-        letterCombinations.letterCombinations("23");
+        List<String> res =letterCombinations.letterCombinations("23");
+        System.out.println();
     }
 }
