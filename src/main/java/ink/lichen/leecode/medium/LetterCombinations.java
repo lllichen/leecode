11e32c2250f1e1aa8e9ff834f84bfc145310e48a
lichen <lichen@daojia-inc.com>
2018-09-19 11:19:24 +0800

    add letterCombination

--- src/main/java/ink/lichen/leecode/medium/LetterCombinations.java
+++ src/main/java/ink/lichen/leecode/medium/LetterCombinations.java
@@ -10,19 +10,23 @@ import java.util.Map;
 public class LetterCombinations {
 
     public List<String> letterCombinations(String digits) {
-        Map<Integer,String> map = new HashMap<>();
-        map.put(2,"abc");
-        map.put(3,"def");
-        map.put(4,"ghi");
-        map.put(5,"jkl");
-        map.put(6,"mno");
-        map.put(7,"pqrs");
-        map.put(8,"tuv");
-        map.put(9,"wxyz");
+        Map<Character,String> map = new HashMap<>();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
+
+//       List
 
         for (int i = 0; i < digits.length(); i++)
         {
             char c = digits.charAt(i);
+            String val = map.get(c);
+
         }
 
         return null;
