e0ca3107965a2a29bf48dfd7ee207bdb46a15c51
lichen <lichen@daojia-inc.com>
2018-09-20 19:12:58 +0800

    add GeneratorParenthesis

--- src/main/java/ink/lichen/leecode/medium/GenerateParenthesis.java
+++ src/main/java/ink/lichen/leecode/medium/GenerateParenthesis.java
@@ -7,12 +7,33 @@ import java.util.List;
  */
 public class GenerateParenthesis {
 
+
+    private String add1(String val){
+        return val+"()";
+    }
+
+    private String add2(String val){
+        return "()"+val;
+    }
+
+    private String add3(String val){
+        return "("+val+")";
+    }
+
+
+
     public List<String> generateParenthesis(int n) {
 
-        for (int i =0 ; i < n ; i++){
-            
-        }
+        String start = "";
 
         return null;
+    }
+
+    public void help(int i,int z, List<String> result, String val, int n){
+        if (i == n){
+            return;
+        }else {
+            add1(val);
+        }
     }
 }
