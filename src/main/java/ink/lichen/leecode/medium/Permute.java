31f60eea5f7917d39c565f85cf802b9b21a26fa5
lichen <lichen@daojia-inc.com>
2018-09-21 18:31:54 +0800

    add Permute

---
+++ src/main/java/ink/lichen/leecode/medium/Permute.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.medium;
+
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-9-21.
+ */
+public class Permute {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        return null;
+    }
+}
