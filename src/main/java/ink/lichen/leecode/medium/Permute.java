39b36cbc919681c6054e98e605ddec2fbe43b393
lichen <lichen@daojia-inc.com>
2018-09-25 11:22:00 +0800

    fix eror

--- src/main/java/ink/lichen/leecode/medium/Permute.java
+++ src/main/java/ink/lichen/leecode/medium/Permute.java
@@ -1,14 +1,35 @@
 package ink.lichen.leecode.medium;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by lichen@daojia.com on 2018-9-21.
+ *
+ * 输入: [1,2,3]
+     输出:
+     [
+     [1,2,3],
+     [1,3,2],
+     [2,1,3],
+     [2,3,1],
+     [3,1,2],
+     [3,2,1]
+     ]
+ *
  */
 public class Permute {
 
     public List<List<Integer>> permute(int[] nums) {
-
-        return null;
+        List list = new ArrayList();
+        for (int val : nums){
+            list.add(val);
+        }
+        return list;
     }
+
+    public void help(){
+
+    }
+
 }
