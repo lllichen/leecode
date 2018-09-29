bbd3305074f4794a8cb0d91767d3b59c2d097cb8
lichen <lichen@daojia-inc.com>
2018-09-29 18:03:55 +0800

    update

--- src/main/java/ink/lichen/leecode/medium/Permute.java
+++ src/main/java/ink/lichen/leecode/medium/Permute.java
@@ -1,7 +1,6 @@
 package ink.lichen.leecode.medium;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -23,8 +22,16 @@ public class Permute {
 
     public List<List<Integer>> permute(int[] nums) {
 
-        List list = subsets(nums);
-        return list;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+
+
+        for (int i =0 ; i < nums.length; i++){
+
+
+        }
+
+        return res;
     }
 
     public void help(List<List<Integer>> list , List<Integer> numList,int[] nums)
@@ -43,27 +50,27 @@ public class Permute {
             help(list,newList,nums);
         }
     }
+//
+//    public List<List<Integer>> subsets(int[] nums) {
+//        List<List<Integer>> list = new ArrayList<>();
+//        Arrays.sort(nums);
+//        backtrack(list, new ArrayList<>(), nums, 0);
+//        return list;
+//    }
 
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
-        backtrack(list, new ArrayList<>(), nums, 0);
-        return list;
-    }
-
-    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-        list.add(new ArrayList<>(tempList));
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, i + 1);
-            tempList.remove(tempList.size() - 1);
-        }
-    }
+//    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+//        list.add(new ArrayList<>(tempList));
+//        for(int i = start; i < nums.length; i++){
+//            tempList.add(nums[i]);
+//            backtrack(list, tempList, nums, i + 1);
+//            tempList.remove(tempList.size() - 1);
+//        }
+//    }
 
     public static void main(String[] args) {
         int [] a = new int[]{1,2,3};
         Permute permute = new Permute();
-        System.out.println(permute.subsets(a));
+//        permute.help();
     }
 
 }
