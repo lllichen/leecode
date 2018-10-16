6da67da307f8ee7d0f3f6e2e975dc137d0c59dab
lichen <lichen@daojia-inc.com>
2018-10-16 16:08:26 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/medium/Subsets.java
@@ -0,0 +1,16 @@
+package ink.lichen.leecode.medium;
+
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-16.
+ */
+public class Subsets {
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        return null;
+    }
+
+
+}
