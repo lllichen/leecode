a0516d38211a9ebdf52727a8e3010b169a2f26b6
lichen <lichen@daojia-inc.com>
2018-10-17 11:39:16 +0800

    add comment

--- src/main/java/ink/lichen/leecode/medium/Subsets.java
+++ src/main/java/ink/lichen/leecode/medium/Subsets.java
@@ -4,6 +4,21 @@ import java.util.List;
 
 /**
  * Created by lichen@daojia.com on 2018-10-16.
+ *
+ * Input: nums = [1,2,3]
+     Output:
+     [
+     [3],
+     [1],
+     [2],
+     [1,2,3],
+     [1,3],
+     [2,3],
+     [1,2],
+     []
+     ]
+ *
+ *
  */
 public class Subsets {
 
