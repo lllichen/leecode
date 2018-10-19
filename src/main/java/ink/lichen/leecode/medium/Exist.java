20e9de32ea5f521d333e6cc85a983be895f25e45
lichen <lichen@daojia-inc.com>
2018-10-19 19:15:55 +0800

    add Exist

---
+++ src/main/java/ink/lichen/leecode/medium/Exist.java
@@ -0,0 +1,24 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-19.
+ *
+ * board =
+ [
+ ['A','B','C','E'],
+ ['S','F','C','S'],
+ ['A','D','E','E']
+ ]
+
+ 给定 word = "ABCCED", 返回 true.
+ 给定 word = "SEE", 返回 true.
+ 给定 word = "ABCB", 返回 false.
+ *
+ */
+public class Exist {
+
+    public boolean exist(char[][] board, String word) {
+
+        return false;
+    }
+}
