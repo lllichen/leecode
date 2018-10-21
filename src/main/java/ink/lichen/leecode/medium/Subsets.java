5a4baee52dd1692bd6d4a0021116086bc63a61e6
Li Chen <lichen@teleinfo.cn>
2018-10-21 21:43:37 +0800

    fix error

--- src/main/java/ink/lichen/leecode/medium/Subsets.java
+++ src/main/java/ink/lichen/leecode/medium/Subsets.java
@@ -22,59 +22,44 @@ import java.util.List;
  */
 public class Subsets {
 
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        result.add(new ArrayList<>());
-
-        for (int i : nums){
-            List<List<Integer>> tmp = new ArrayList<>();
-
-            for(List temp : result){
-                tmp.add(temp);
-            }
-            for (List temp : result){
-                List tm = new ArrayList<>();
-                tm.addAll(temp);
-                tm.add(i);
-               tmp.add(tm);
-            }
-            result = tmp;
-        }
-
-        return result;
-    }
-
-    //    public List<List<Integer>> subsets(int[] nums) {
-//
+//    public List<List<Integer>> subsets(int[] nums) {
 //        List<List<Integer>> result = new ArrayList<>();
-//        this.backtrack(result,new ArrayList<>(),nums);
+//
+//        result.add(new ArrayList<>());
+//
+//        for (int i : nums) {
+//            List<List<Integer>> tmp = new ArrayList<>();
+//
+//            for (List temp : result) {
+//                tmp.add(temp);
+//            }
+//            for (List temp : result) {
+//                List tm = new ArrayList<>();
+//                tm.addAll(temp);
+//                tm.add(i);
+//                tmp.add(tm);
+//            }
+//            result = tmp;
+//        }
 //
 //        return result;
 //    }
-//
-//    public void backtrack(List<List<Integer>> result,List<Integer> temp,int[] nums){
-//        if (!result.contains(temp)){
-//
-//        }
-//    }
 
-//    public List<List<Integer>> subsets(int[] nums) {
-//        List<List<Integer>> res = new ArrayList<List<Integer>>();
-//        backTrack(res, new ArrayList<>(), nums, 0);
-//        return res;
-//    }
-//
-//    public void backTrack(List<List<Integer>> res, List<Integer> tempList, int[] nums, int n) {
-//        if (tempList.size() <= n) {
-//            res.add(new ArrayList<Integer>(tempList));
-//        }
-//        for (int i = n; i < nums.length; i++) {
-//            tempList.add(nums[i]);
-//            backTrack(res, tempList, nums, i + 1);
-//            tempList.remove(tempList.size() - 1);
-//        }
-//    }
+    public List<List<Integer>> subsets(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        backtrace(res,new ArrayList<Integer>(),nums,0);
+        return res;
+    }
+
+    public void backtrace(List<List<Integer>> res, List<Integer> tempList, int[] nums,int n){
+        res.add(tempList);
+        for (int i = n ; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrace(res,tempList,nums,i+1);
+            tempList.remove(tempList.size()-1);
+        }
+    }
+
     public static void main(String[] args) {
         Subsets subsets = new Subsets();
         subsets.subsets(new int[]{1,2,3,4});
