e9dfe08d3c531e20af5093a76c60f6bf7ac965c6
lichen <lichen@daojia-inc.com>
2018-10-24 19:07:40 +0800

    update code

---
+++ src/main/java/ink/lichen/leecode/medium/FindKthLargest.java
@@ -0,0 +1,18 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-24.
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+   输出: 5
+ */
+public class FindKthLargest {
+
+
+    public int findKthLargest(int[] nums, int k) {
+        for (int i = 0 ; i < nums.length ; i++)
+        {
+
+        }
+        return 0;
+    }
+}
