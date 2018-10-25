3025a36531a122536f23a5080715d37bc4d7f413
lichen <lichen@daojia-inc.com>
2018-10-25 10:32:30 +0800

    add file

--- src/main/java/ink/lichen/leecode/medium/FindPeakElement.java
+++ src/main/java/ink/lichen/leecode/medium/FindPeakElement.java
@@ -2,6 +2,11 @@ package ink.lichen.leecode.medium;
 
 /**
  * Created by lichen@daojia.com on 2018-10-25.
+ *
+ * 输入: nums = [1,2,3,1]
+ * 输出: 2
+ * 解释: 3 是峰值元素，你的函数应该返回其索引 2。
+ *
  */
 public class FindPeakElement {
 }
