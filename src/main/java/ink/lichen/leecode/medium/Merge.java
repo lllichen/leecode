a35429378c79ffa4de84e09648321dee98d6aedc
lichen <lichen@daojia-inc.com>
2018-10-25 16:51:13 +0800

    add merge annd support

---
+++ src/main/java/ink/lichen/leecode/medium/Merge.java
@@ -0,0 +1,17 @@
+package ink.lichen.leecode.medium;
+
+import ink.lichen.leecode.support.Interval;
+
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-25.
+ */
+public class Merge {
+
+
+    public List<Interval> merge(List<Interval> intervals) {
+
+        return null;
+    }
+}
---
+++ src/main/java/ink/lichen/leecode/support/Interval.java
@@ -0,0 +1,19 @@
+package ink.lichen.leecode.support;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-25.
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
