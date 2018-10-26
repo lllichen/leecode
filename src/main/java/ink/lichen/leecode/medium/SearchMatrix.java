3fdce8a1f9cfbba00cbe90410e71e42776c18c6a
lichen <lichen@daojia-inc.com>
2018-10-26 14:49:28 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/medium/SearchMatrix.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-26.
+ */
+public class SearchMatrix {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        return false;
+    }
+}
