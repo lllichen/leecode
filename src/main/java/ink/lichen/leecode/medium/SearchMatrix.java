25e94d38949ed573191c905af94f436b75b46cdc
lichen <lichen@daojia-inc.com>
2018-10-26 16:47:19 +0800

    add new code

--- src/main/java/ink/lichen/leecode/medium/SearchMatrix.java
+++ src/main/java/ink/lichen/leecode/medium/SearchMatrix.java
@@ -23,13 +23,18 @@ package ink.lichen.leecode.medium;
 public class SearchMatrix {
 
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i =  0; i < matrix.length; i++){
-            for (int j = 0 ; j <matrix[0].length; j++){
-                if (matrix[i][j] == target){
-                    return true;
-                }
-            }
+//     for (int i =  0; i < matrix.length; i++){
+//          for (int j = 0 ; j <matrix[0].length; j++){
+//                if (matrix[i][j] == target){
+//                    return true;
+//                }
+//            }
+//        }
+//      return false;
+        for (int i = 0 ; i < matrix.length; i++)
+        {
+
         }
-        return false;
+
     }
 }
