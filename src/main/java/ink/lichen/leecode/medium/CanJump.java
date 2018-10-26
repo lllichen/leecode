7233fa351c6015c379ca069295fc28c3e473db4f
lichen <lichen@daojia-inc.com>
2018-10-26 17:26:37 +0800

    add can jumb

---
+++ src/main/java/ink/lichen/leecode/medium/CanJump.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-26.
+ */
+public class CanJump {
+
+    public boolean canJump(int[] nums) {
+
+        return false;
+    }
+}
