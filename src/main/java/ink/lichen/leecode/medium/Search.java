e325f13576b94a641241458f63502e350d7b4bbc
Li Chen <lichen@teleinfo.cn>
2018-10-26 08:40:07 +0800

    add info

--- src/main/java/ink/lichen/leecode/medium/Search.java
+++ src/main/java/ink/lichen/leecode/medium/Search.java
@@ -3,7 +3,23 @@ package ink.lichen.leecode.medium;
 public class Search {
 
     public int search(int[] nums, int target) {
+        int flag = nums[0];
+
+        if (target == flag){
+            return 0;
+        }
+        int n = nums.length;
+        int lo=0,high=n-1;
+        while (lo <= high){
+            int mid = lo + high;
+        }
+
+
+
+
 
         return 0;
     }
+
+
 }
