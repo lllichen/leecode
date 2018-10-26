65945bf9fb82340ecaf87b18ef7491cb4ee4d02d
lichen <lichen@daojia-inc.com>
2018-10-26 14:51:23 +0800

    searchMatrix

--- src/main/java/ink/lichen/leecode/medium/SearchMatrix.java
+++ src/main/java/ink/lichen/leecode/medium/SearchMatrix.java
@@ -2,6 +2,24 @@ package ink.lichen.leecode.medium;
 
 /**
  * Created by lichen@daojia.com on 2018-10-26.
+ *
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
+ *
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ *
+ * [
+ *  [1,   4,  7, 11, 15],
+ *  [2,   5,  8, 12, 19],
+ *  [3,   6,  9, 16, 22],
+ *  [10, 13, 14, 17, 24],
+ *  [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ *
+ * 给定 target = 20，返回 false。
+ *
+ *
  */
 public class SearchMatrix {
 
