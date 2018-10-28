b8ff92ec179566396638eaf0727cc6a2c4ecc659
Li Chen <lichen@teleinfo.cn>
2018-10-28 20:43:15 +0800

    add dp

--- src/main/java/ink/lichen/leecode/medium/UniquePaths.java
+++ src/main/java/ink/lichen/leecode/medium/UniquePaths.java
@@ -1,8 +1,33 @@
 package ink.lichen.leecode.medium;
 
 public class UniquePaths {
-    public int uniquePaths(int m, int n) {
+    private int [][] dp;
+    private int m;
+    private int n;
 
-        return 0;
+
+
+    public int uniquePaths(int m, int n) {
+        this.dp = new int[m][n];
+        this.m = m;
+        this.n = n;
+        return dfs(0,0);
+    }
+
+    private int dfs(int x,int y) {
+        if (x > m-1 || y > n - 1) {
+            return 0;
+        }
+        if (x == m - 1 && y == n - 1) {
+            return 1;
+        }
+        if (dp[x][y] == 0)
+            dp[x][y] = dfs(x + 1, y) + dfs(x, y + 1);
+        return dp[x][y];
+    }
+
+    public static void main(String[] args) {
+        UniquePaths uniquePaths = new UniquePaths();
+        System.out.println(uniquePaths.uniquePaths(7,9));
     }
 }
