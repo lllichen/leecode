10df785378f1e17729f6c8e1fc4fe99e56a94540
Li Chen <lichen@teleinfo.cn>
2018-10-28 16:02:26 +0800

    fix errir

--- src/main/java/ink/lichen/leecode/medium/CanJump.java
+++ src/main/java/ink/lichen/leecode/medium/CanJump.java
@@ -2,11 +2,35 @@ package ink.lichen.leecode.medium;
 
 /**
  * Created by lichen@daojia.com on 2018-10-26.
+ * 输入: [2,3,1,1,4]
+ * 输出: true
  */
 public class CanJump {
 
     public boolean canJump(int[] nums) {
 
-        return false;
+        int n = nums.length;
+        if (n == 1){
+            return true;
+        }
+
+        int max = 0;
+        for (int i = 0 ; i < n ; i++)
+        {
+            int val = nums[i];
+            if (val > max){
+                max = val;
+            }
+            if (max <= 0 && i != n -1 ){
+                return false;
+            }
+            max--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        CanJump canJump = new CanJump();
+        System.out.println( canJump.canJump(new int[]{2,0,0}));
     }
 }
---
+++ src/main/java/ink/lichen/leecode/medium/UniquePaths.java
@@ -0,0 +1,8 @@
+package ink.lichen.leecode.medium;
+
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+
+        return 0;
+    }
+}
