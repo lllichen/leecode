66460d72a853449f7e06ae9b85e5b53dd555993b
lichen <lichen@daojia-inc.com>
2018-10-29 09:29:43 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/medium/CoinChange.java
@@ -0,0 +1,16 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-29.
+ * 给定不同面额的硬币 coins 和一个总金额 amount。
+ * 编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+ * 如果没有任何一种硬币组合能组成总金额，返回 -1。
+ */
+public class CoinChange {
+
+
+    public int coinChange(int[] coins, int amount) {
+
+        return 0;
+    }
+}
