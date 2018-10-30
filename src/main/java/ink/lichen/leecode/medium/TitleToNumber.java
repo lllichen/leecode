d41b5a89e7aaad0a5f949d57d75fd08e1d9d428c
lichen <lichen@daojia-inc.com>
2018-10-30 20:59:51 +0800

    add all code

---
+++ src/main/java/ink/lichen/leecode/medium/TitleToNumber.java
@@ -0,0 +1,17 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-30.
+ * 给定一个Excel表格中的列名称，返回其相应的列序号。
+ * 输入: "A"
+ * 输出: 1
+ */
+public class TitleToNumber {
+
+    public int titleToNumber(String s) {
+        for (int i = 0 ; i < s.charAt(i); i++ ){
+
+        }
+        return 0;
+    }
+}
