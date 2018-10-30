171a6e8957190d3269980f32899571de39bd7bb1
Li Chen <lichen@teleinfo.cn>
2018-10-30 08:47:06 +0800

    add longestPalindrome

--- src/main/java/ink/lichen/leecode/medium/LongestPalindrome.java
+++ src/main/java/ink/lichen/leecode/medium/LongestPalindrome.java
@@ -10,14 +10,44 @@ public class LongestPalindrome {
 
     public String longestPalindrome(String s) {
 
-        char[] chars = new char[1000];
-        int max = 0;
-        for (int i = 1 ;i < s.length(); i++){
-            if ( ( i+1 < s.length() ) && s.charAt(i-1) == s.charAt(i+1) ) {
-                System.out.println(i);
+        if (s == null || s.length() < 2)
+            return s;
+
+        // 添加头^尾$两个不同的字符用于消除边界判断
+        String temp = "^" + s + "$";
+
+        int c = 0, r = 0, len = s.length() * 2 + 1 + 2, centerIndex = 0, maxLen = 0;
+        int[] p = new int[len];
+
+        for (int i = 1; i < len - 1; i++) {
+            int iMirror = 2 * c - i;
+
+            p[i] = r > i ? Math.min(r - i, p[iMirror]) : 0;
+
+            // 基于当前点为中心扩展寻找回文
+            int left = i - 1 - p[i];
+            int right = i + 1 + p[i];
+            while (temp.charAt(left / 2) == temp.charAt((right - 1) / 2)) {
+                p[i]++;
+                left--;
+                right++;
+            }
+
+            // 如果扩展后的右边界值大于当前右边界值则更新
+            if (i + p[i] > r) {
+                c = i;
+                r = i + p[i];
+            }
+
+            // 寻找最大值与中心点
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIndex = i;
             }
         }
 
-        return new String(chars);
+        int beginIndex = (centerIndex - 1 - maxLen) / 2;
+
+        return s.substring(beginIndex, beginIndex + maxLen);
     }
 }
