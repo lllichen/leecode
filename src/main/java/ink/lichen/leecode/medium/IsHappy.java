8d9f1ff4c4a0471cf01c803e57ae98d0c569a6e0
lichen <lichen@daojia-inc.com>
2018-10-30 18:26:25 +0800

    update code

--- src/main/java/ink/lichen/leecode/medium/IsHappy.java
+++ src/main/java/ink/lichen/leecode/medium/IsHappy.java
@@ -1,21 +1,36 @@
 package ink.lichen.leecode.medium;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by lichen@daojia.com on 2018-10-30.
  * 输入: 19
  * 输出: true
  * 解释:
- * 12 + 92 = 82
- * 82 + 22 = 68
- * 62 + 82 = 100
- * 12 + 02 + 02 = 1
+ * 1*1 + 9*9 = 82
+ * 8*8 + 2*2 = 68
+ * 6*6 + 8*8 = 100
+ * 1*1 + 0*0 + 0*0 = 1
  */
 public class IsHappy {
 
 
     public boolean isHappy(int n) {
-
-
-        return false;
+        Set<Integer> set = new HashSet<Integer>();
+        while(n!=1){
+            int sum = 0;
+            while(n>0){
+                sum += (n % 10) * (n % 10);
+                n = n / 10;
+            }
+            if(set.contains(sum)){
+                return false;
+            } else {
+                set.add(sum);
+            }
+            n = sum;
+        }
+        return true;
     }
 }
