2395a6736546bb2ca22eadd4b89d67dcc04fe22c
lichen <lichen@daojia-inc.com>
2018-10-30 18:16:10 +0800

    fix error

--- src/main/java/ink/lichen/leecode/medium/IsHappy.java
+++ src/main/java/ink/lichen/leecode/medium/IsHappy.java
@@ -2,6 +2,20 @@ package ink.lichen.leecode.medium;
 
 /**
  * Created by lichen@daojia.com on 2018-10-30.
+ * 输入: 19
+ * 输出: true
+ * 解释:
+ * 12 + 92 = 82
+ * 82 + 22 = 68
+ * 62 + 82 = 100
+ * 12 + 02 + 02 = 1
  */
 public class IsHappy {
+
+
+    public boolean isHappy(int n) {
+
+
+        return false;
+    }
 }
