46830456da486d02951448bc33795aa0774265bd
lichen <lichen@daojia-inc.com>
2018-10-31 16:54:39 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/medium/GetSum.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-31.
+ *
+ * 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+ */
+public class GetSum {
+    public int getSum(int a, int b) {
+
+        return 0;
+    }
+}
