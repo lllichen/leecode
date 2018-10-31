9f89b8005a0c881b54d68b2ba8d8de8e99098b6a
lichen <lichen@daojia-inc.com>
2018-10-31 15:32:53 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/medium/FractionToDecimal.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-10-31.
+ */
+public class FractionToDecimal {
+
+    public String fractionToDecimal(int numerator, int denominator) {
+
+        return "";
+    }
+}
