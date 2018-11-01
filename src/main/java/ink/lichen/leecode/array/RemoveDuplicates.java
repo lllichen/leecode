f21729df93bf89af8fda0f262c16baa7b77e2232
lichen <lichen@daojia-inc.com>
2018-11-01 16:06:40 +0800

    add removeDuplicates

--- src/main/java/ink/lichen/leecode/array/RemoveDuplicates.java
+++ src/main/java/ink/lichen/leecode/array/RemoveDuplicates.java
@@ -7,11 +7,22 @@ public class RemoveDuplicates {
 
 
     public static int removeDuplicates(int[] nums) {
+
+        int n = nums.length;
+        if (n == 0 || n == 1){
+            return n;
+        }
         int i = 0;
 
-        for (int j = 0 ;i < nums.length; j++ )
+        for (int j = 1 ;j < nums.length; j++ )
         {
-
+            if (nums[j] != nums[i])
+            {
+                if (j != i+1){
+                    nums[i+1] = nums[j];
+                }
+                i++;
+            }
         }
 
         return i +1;
