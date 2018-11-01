6c9932a855781e027d4bb0aff51cd6e33b2bc131
lichen <lichen@daojia-inc.com>
2018-11-01 11:52:34 +0800

    add leastInterval

---
+++ src/main/java/ink/lichen/leecode/medium/LeastInterval.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.medium;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-1.
+ */
+public class LeastInterval {
+
+    public int leastInterval(char[] tasks, int n) {
+
+        return 0;
+    }
+}
--- src/main/java/ink/lichen/leecode/medium/MajorityElement.java
+++ src/main/java/ink/lichen/leecode/medium/MajorityElement.java
@@ -1,5 +1,8 @@
 package ink.lichen.leecode.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by lichen@daojia.com on 2018-11-1.
  *
@@ -15,7 +18,21 @@ package ink.lichen.leecode.medium;
 public class MajorityElement {
 
     public int majorityElement(int[] nums) {
-
+        Map<Integer,Integer> res = new HashMap<>();
+        int n = nums.length;
+        int val = n /2;
+        for (int t : nums){
+            res.put(t, res.getOrDefault(t,0)+1);
+            if (res.get(t) > val){
+                return t;
+            }
+        }
         return 0;
+    }
+
+    public static void main(String[] args) {
+        MajorityElement majorityElement = new MajorityElement();
+        majorityElement.majorityElement(new int[]{3,2,3});
+
     }
 }
