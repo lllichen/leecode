bc63bf87166fb40aebf2dc1455b9fabfced977dc
lichen <lichen@daojia-inc.com>
2018-11-01 11:34:51 +0800

    fix error

--- src/main/java/ink/lichen/leecode/medium/MajorityElement.java
+++ src/main/java/ink/lichen/leecode/medium/MajorityElement.java
@@ -4,4 +4,9 @@ package ink.lichen.leecode.medium;
  * Created by lichen@daojia.com on 2018-11-1.
  */
 public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+
+        return 0;
+    }
 }
