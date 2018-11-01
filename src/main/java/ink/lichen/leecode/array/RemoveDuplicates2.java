7b7764015eb94dce6e55fa2ea58897732832a4b8
lichen <lichen@daojia-inc.com>
2018-11-01 17:37:43 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/array/RemoveDuplicates2.java
@@ -0,0 +1,7 @@
+package ink.lichen.leecode.array;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-1.
+ */
+public class RemoveDuplicates2 {
+}
--- src/main/java/ink/lichen/leecode/easy/array/RemoveDuplicates.java
+++ src/main/java/ink/lichen/leecode/easy/array/RemoveDuplicates.java
@@ -6,14 +6,21 @@ package ink.lichen.leecode.easy.array;
 public class RemoveDuplicates {
 
     public static int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0 || n == 1 )
+        {
+            return n;
+        }
         int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-
-            if (nums[i] != nums[j]){
+        for (int j = 1 ; j < n ; j++){
+            if (nums[i] != nums[j])
+            {
+                if (j != i+1)
+                {
+                    nums[i+1] = nums[j];
+                }
                 i++;
-                nums[i] = nums[j];
             }
-
         }
         return i +1;
     }
