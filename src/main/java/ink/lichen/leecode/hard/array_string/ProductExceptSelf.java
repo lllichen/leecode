cc0fe73bc21e9d73ff978eea1d9e70d1e9a5db63
lichen <lichen@daojia-inc.com>
2018-11-01 12:28:15 +0800

    add hard code

---
+++ src/main/java/ink/lichen/leecode/hard/array_string/ProductExceptSelf.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.hard.array_string;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-1.
+ */
+public class ProductExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+
+        return null;
+    }
+}
