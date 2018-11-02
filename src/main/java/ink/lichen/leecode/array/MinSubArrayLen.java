8758bdfec186173b59fb7c48ebb2f228f867c76f
lichen <lichen@daojia-inc.com>
2018-11-02 17:38:24 +0800

    add comment

--- src/main/java/ink/lichen/leecode/array/MinSubArrayLen.java
+++ src/main/java/ink/lichen/leecode/array/MinSubArrayLen.java
@@ -2,6 +2,11 @@ package ink.lichen.leecode.array;
 
 /**
  * Created by lichen@daojia.com on 2018-11-2.
+ *
+ * 输入: s = 7, nums = [2,3,1,2,4,3]
+ * 输出: 2
+ * 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
+ *
  */
 public class MinSubArrayLen {
 
