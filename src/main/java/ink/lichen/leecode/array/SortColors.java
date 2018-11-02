332ea7ad5fe41658f54e5e7ffc74e5b34cc8a9e8
lichen <lichen@daojia-inc.com>
2018-11-02 16:52:42 +0800

    fix error

--- src/main/java/ink/lichen/leecode/array/SortColors.java
+++ src/main/java/ink/lichen/leecode/array/SortColors.java
@@ -9,21 +9,22 @@ public class SortColors {
 
     public void sortColors(int[] nums) {
 
-        int red = 0, n = nums.length;
-        int blue = n;
-
-        for (int i = 0 ; i < blue; i++)
-        {
-            if (nums[i] == 0){
-                int temp = nums[i];
-                nums[i] = nums[red];
-                nums[++red] = temp;
-            }else if (nums[i] == 2){
-                int temp = nums[i];
-                nums[i] = nums[blue];
-                nums[--blue] = temp;
-            }
-        }
+      int red_idx = 0;
+      int blue_idx = nums.length -1;
+      for (int i = 0 ; i <= blue_idx;i++ )
+      {
+          if (nums[i] == 0)
+          {
+              int temp = nums[i];
+              nums[i] = nums[red_idx];
+              nums[red_idx++] = temp;
+          }else if (nums [i] == 2)
+          {
+              int temp = nums[i];
+              nums[i--] = nums[blue_idx];
+              nums[blue_idx--] = temp;
+          }
+      }
 
     }
 
