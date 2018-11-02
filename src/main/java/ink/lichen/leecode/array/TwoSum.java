76662ba23dfdf645112dd16e09a563166b170866
lichen <lichen@daojia-inc.com>
2018-11-02 16:55:04 +0800

    add twoSum

--- src/main/java/ink/lichen/leecode/array/TwoSum.java
+++ src/main/java/ink/lichen/leecode/array/TwoSum.java
@@ -4,4 +4,12 @@ package ink.lichen.leecode.array;
  * Created by lichen@daojia.com on 2018-11-2.
  */
 public class TwoSum {
+
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        return null;
+    }
 }
+
+
