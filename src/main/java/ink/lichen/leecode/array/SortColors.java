4b69fd4d115ffdcff1706cb938c8559d4df0bec9
lichen <lichen@daojia-inc.com>
2018-11-02 10:46:35 +0800

    fix sortColor

--- src/main/java/ink/lichen/leecode/array/SortColors.java
+++ src/main/java/ink/lichen/leecode/array/SortColors.java
@@ -4,4 +4,16 @@ package ink.lichen.leecode.array;
  * Created by lichen@daojia.com on 2018-11-2.
  */
 public class SortColors {
+
+
+
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int blue = 2;
+        for (int i = 0 ; i < nums.length; i++)
+        {
+
+        }
+
+    }
 }
