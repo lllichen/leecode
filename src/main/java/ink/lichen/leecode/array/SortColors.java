634af5692db342b044e72be4f604d3c13058b6b1
lichen <lichen@daojia-inc.com>
2018-11-02 11:37:23 +0800

    add something

--- src/main/java/ink/lichen/leecode/array/SortColors.java
+++ src/main/java/ink/lichen/leecode/array/SortColors.java
@@ -8,12 +8,22 @@ public class SortColors {
 
 
     public void sortColors(int[] nums) {
-        int red = 0;
-        int blue = 2;
-        for (int i = 0 ; i < nums.length; i++)
-        {
 
+        int red = 0,n = nums.length;
+        int blue = n-1;
+        for (int i = 0 ; i < blue; i++)
+        {
+            if (nums[i] == 0){
+                int temp = nums[i];
+                nums[i] = nums[red];
+                nums[red++] = temp;
+            }else if (nums[i] == 2){
+                int temp = nums[i];
+                nums[i] = nums[blue];
+                nums[blue--] = temp;
+            }
         }
 
     }
+
 }
