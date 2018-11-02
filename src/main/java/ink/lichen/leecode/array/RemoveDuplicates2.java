436912cfdf716a348f8c33972171abf87a3bbdc4
lichen <lichen@daojia-inc.com>
2018-11-02 10:44:39 +0800

    add setColor

--- src/main/java/ink/lichen/leecode/array/RemoveDuplicates2.java
+++ src/main/java/ink/lichen/leecode/array/RemoveDuplicates2.java
@@ -8,23 +8,12 @@ public class RemoveDuplicates2 {
 
 
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        if (n == 0 || n == 1 || n == 2)
-        {
-            return n;
-        }
-        int i = 0;
-
-        int val = 0;
-        for (int j = 1 ; j < n; j++ )
-        {
-
-            if (nums[j] != nums[i]&& j == i+1) {
-                nums[i + 1] = nums[j];
+        int n = 0;
+        for (int val : nums){
+            if (n < 2 || val > nums[n-2]){
+                nums[n++] = val;
             }
-            i++;
         }
-
-        return 0;
+        return n;
     }
 }
