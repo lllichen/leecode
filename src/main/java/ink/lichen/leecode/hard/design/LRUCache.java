c37c8bbdc0a687447c1ea9d5eb870bf1b32dae0b
Li Chen <lichen@teleinfo.cn>
2018-11-05 22:13:54 +0800

    add LRU

--- src/main/java/ink/lichen/leecode/hard/design/LRUCache.java
+++ src/main/java/ink/lichen/leecode/hard/design/LRUCache.java
@@ -1,4 +1,32 @@
 package ink.lichen.leecode.hard.design;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class LRUCache {
+
+    Stack<Integer> stack ;
+    Map<Integer, Integer> map ;
+    int capacity;
+
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        stack = new Stack<>();
+    }
+
+    public int get(int key) {
+       return map.getOrDefault(key,-1);
+    }
+
+    public void put(int key, int value) {
+        if (stack.size() == capacity){
+            map.remove(stack.pop());
+        }
+        stack.push(key);
+        map.put(key,value);
+    }
+
 }
