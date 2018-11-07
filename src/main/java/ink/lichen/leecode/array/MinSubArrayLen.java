90a11418afc2b84f3e3afabc5fadaab00d4b17e4
lichen <togetherlichen@gmail.com>
2018-11-08 07:54:56 +0800

    add my code

--- src/main/java/ink/lichen/leecode/array/MinSubArrayLen.java
+++ src/main/java/ink/lichen/leecode/array/MinSubArrayLen.java
@@ -11,7 +11,17 @@ package ink.lichen.leecode.array;
 public class MinSubArrayLen {
 
     public int minSubArrayLen(int s, int[] nums) {
+        int i = 0,j=0,n = nums.length ;
+        int sum = 0;
+        int max = Integer.MAX_VALUE;
+        while (i<n){
+            sum +=nums[i++];
+            while (sum>=s){
+                max = Math.min(max,i - j);
+                sum -=nums[j++];
+            }
+        }
 
-        return 0;
+        return max != Integer.MAX_VALUE?0:max;
     }
 }
