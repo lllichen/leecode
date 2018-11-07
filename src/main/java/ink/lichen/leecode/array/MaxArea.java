d9ee1d98ff5bbee23ce46765f934410551c0491b
lichen <togetherlichen@gmail.com>
2018-11-08 07:41:32 +0800

    fix maxArea

---
+++ src/main/java/ink/lichen/leecode/array/MaxArea.java
@@ -0,0 +1,28 @@
+package ink.lichen.leecode.array;
+
+public class MaxArea {
+
+
+    public int maxArea(int[] height) {
+
+        int max = 0;
+        int n = height.length, lo = 0, hi = n-1;
+        while (lo < hi){
+            if (height[lo] < height[hi]){
+                int val = (hi-lo)*height[lo];
+                if (max < val){
+                    max = val;
+                }
+                lo++;
+            }else if (height[lo] >= height[hi]){
+                int val = (hi-lo)*height[lo];
+                if (max < val){
+                    max = val;
+                }
+                hi--;
+            }
+        }
+        return max;
+
+    }
+}
