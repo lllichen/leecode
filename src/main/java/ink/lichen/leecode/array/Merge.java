b0deab7b985484cf0cf98ee006cefd27f0834b85
lichen <togetherlichen@gmail.com>
2018-11-08 07:57:23 +0800

    add merge

---
+++ src/main/java/ink/lichen/leecode/array/Merge.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.array;
+
+/**
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ */
+public class Merge {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+
+
+    }
+}
