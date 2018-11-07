b68c52d6fc9e6e090967f42fdbcf01fc263b8df2
lichen <lichen@daojia-inc.com>
2018-11-07 18:38:07 +0800

    add maxArea

---
+++ src/main/java/ink/lichen/leecode/array/MaxArea.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.array;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-7.
+ */
+public class MaxArea {
+
+    public int maxArea(int[] height) {
+
+        return 0;
+    }
+}
