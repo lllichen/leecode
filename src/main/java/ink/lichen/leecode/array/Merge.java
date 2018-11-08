c7653eeea9ea1a99c6370711eeee1f7e4d99373a
lichen <togetherlichen@gmail.com>
2018-11-08 08:29:38 +0800

    merge

--- src/main/java/ink/lichen/leecode/array/Merge.java
+++ src/main/java/ink/lichen/leecode/array/Merge.java
@@ -8,7 +8,33 @@ public class Merge {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
+        int idx_1 = m-1;
+        int idx_2 = n-1;
 
+        for (int i= m+n-1 ; i >=0 ;i--){
+            if ( idx_1 < 0 || idx_2 < 0){
+                if (idx_1 < 0){
+                    nums1[i] = nums2[idx_2];
+                    idx_2--;
+                }else{
+                    nums1[i] = nums1[idx_1];
+                    idx_1--;
+                }
+            }else{
+                if (nums1[idx_1]>nums2[idx_2]){
+                    nums1[i] = nums1[idx_1];
+                    idx_1--;
+                }else {
+                    nums1[i] = nums2[idx_2];
+                    idx_2--;
+                }
+            }
+        }
 
+    }
+
+    public static void main(String[] args) {
+        Merge merge = new Merge();
+        merge.merge(new int[]{0},0,new int[]{1},1);
     }
 }
