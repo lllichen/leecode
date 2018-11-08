e4e748b278ad9407a905b154e7f38864a14b0010
lichen <togetherlichen@gmail.com>
2018-11-08 08:41:42 +0800

    add code

--- src/main/java/ink/lichen/leecode/array/Merge.java
+++ src/main/java/ink/lichen/leecode/array/Merge.java
@@ -31,6 +31,9 @@ public class Merge {
 //            }
 //        }
 
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) { nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--]; }
+        while (n >= 0) { nums1[p--] = nums2[n--]; }
     }
 
     public static void main(String[] args) {
