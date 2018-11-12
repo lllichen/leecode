71355a625215e2e8de8a6d3ab38c718529a7cfb5
lichen <lichen@daojia-inc.com>
2018-11-12 19:55:52 +0800

    add MyAtot

--- src/main/java/ink/lichen/leecode/tx/MyAtoi.java
+++ src/main/java/ink/lichen/leecode/tx/MyAtoi.java
@@ -6,7 +6,12 @@ package ink.lichen.leecode.tx;
 public class MyAtoi {
 
     public int myAtoi(String str) {
+        int res = 0;
         int n = str.length();
+        if (n < 1 || ( str.charAt(0) != '-' && !Character.isDigit(str.charAt(0)) ) ){
+            return res;
+        }
+
         for (int i = 0 ; i <n ; i++){
 
         }
