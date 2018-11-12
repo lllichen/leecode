d27de12abd6408e5dcd16685d6b9171a8fa87d03
lichen <lichen@daojia-inc.com>
2018-11-12 19:51:14 +0800

    add MyAtoi

---
+++ src/main/java/ink/lichen/leecode/tx/MyAtoi.java
@@ -0,0 +1,16 @@
+package ink.lichen.leecode.tx;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-12.
+ */
+public class MyAtoi {
+
+    public int myAtoi(String str) {
+        int n = str.length();
+        for (int i = 0 ; i <n ; i++){
+
+        }
+
+        return 121;
+    }
+}
