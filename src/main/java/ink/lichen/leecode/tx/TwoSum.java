f9ace62be716b8d6411d536ee8f6b98e8ce679b2
lichen <lichen@daojia-inc.com>
2018-11-12 09:37:08 +0800

    add twoSum

---
+++ src/main/java/ink/lichen/leecode/tx/TwoSum.java
@@ -0,0 +1,34 @@
+package ink.lichen.leecode.tx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-12.
+ */
+public class TwoSum {
+
+
+
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+
+                return new int[] { map.get(complement), i };
+
+            }
+
+            map.put(nums[i], i);
+
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+
+    }
+}
