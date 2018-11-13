ca418457d4ea393db0cdcb943e8e08f0cbf67a08
lichen <togetherlichen@gmail.com>
2018-11-13 21:56:25 +0800

    add k list

--- src/main/java/ink/lichen/leecode/tx/MergeKLists.java
+++ src/main/java/ink/lichen/leecode/tx/MergeKLists.java
@@ -6,6 +6,46 @@ public class MergeKLists {
 
 
 
+//    public ListNode mergeKLists(ListNode[] lists) {
+//        ListNode dummy = new ListNode(0);
+//        ListNode cur = dummy;
+//        if (lists.length==0){
+//            return dummy.next;
+//        }else if (lists.length == 1){
+//            return lists[0];
+//        }
+//        int  n = lists.length;
+//
+//
+//        while (true){
+//            ListNode min = null;
+//            int temp = -1;
+//            for (int i = 0; i < n; i++){
+//                if ( lists[i]!= null ){
+//                    if (min == null ){
+//                        min = lists[i];
+//                        temp = i;
+//                    }else if (min != null && min.val > lists[i].val){
+//                        min = lists[i];
+//                        temp = i;
+//                    }
+//                }
+//
+//            }
+//            if (temp != -1){
+//                lists[temp] = lists[temp].next;
+//                cur.next = min;
+//                cur = cur.next;
+//            }else {
+//                break;
+//            }
+//        }
+//
+//        return dummy.next;
+//
+//    }
+
+    // 分治法
     public ListNode mergeKLists(ListNode[] lists) {
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
@@ -16,30 +56,6 @@ public class MergeKLists {
         }
         int  n = lists.length;
 
-
-        while (true){
-            ListNode min = null;
-            int temp = -1;
-            for (int i = 0; i < n; i++){
-                if ( lists[i]!= null ){
-                    if (min == null ){
-                        min = lists[i];
-                        temp = i;
-                    }else if (min != null && min.val > lists[i].val){
-                        min = lists[i];
-                        temp = i;
-                    }
-                }
-
-            }
-            if (temp != -1){
-                lists[temp] = lists[temp].next;
-                cur.next = min;
-                cur = cur.next;
-            }else {
-                break;
-            }
-        }
 
         return dummy.next;
 
