91bfcfa345dec6e3cfe01f3b086154f7622eef99
lichen <lichen@daojia-inc.com>
2018-11-13 12:35:47 +0800

    add LongestPalindrome

--- src/main/java/ink/lichen/leecode/tx/LongestPalindrome.java
+++ src/main/java/ink/lichen/leecode/tx/LongestPalindrome.java
@@ -5,5 +5,29 @@ package ink.lichen.leecode.tx;
  */
 public class LongestPalindrome {
 
+    public String longestPalindrome(String s) {
+        char[] ca = s.toCharArray();
+        int rs = 0, re = 0;
+        int max = 0;
+        for(int i = 0; i < ca.length; i++) {
+            if(isPalindrome(ca, i - max - 1, i)) {
+                rs = i - max - 1; re = i;
+                max += 2;
+            } else if(isPalindrome(ca, i - max, i)) {
+                rs = i - max; re = i;
+                max += 1;
+            }
+        }
+        return s.substring(rs, re + 1);
+    }
+
+    private boolean isPalindrome(char[] ca, int s, int e) {
+        if(s < 0) return false;
+
+        while(s < e) {
+            if(ca[s++] != ca[e--]) return false;
+        }
+        return true;
+    }
 
 }
