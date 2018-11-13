8fc0e969a1b40dceaff26eb613811aff61a3172c
lichen <lichen@daojia-inc.com>
2018-11-13 18:04:21 +0800

    removeDuplicates

--- src/main/java/ink/lichen/leecode/tx/RemoveDuplicates.java
+++ src/main/java/ink/lichen/leecode/tx/RemoveDuplicates.java
@@ -7,16 +7,28 @@ public class RemoveDuplicates {
 
 
     public static int removeDuplicates(int[] nums) {
-        int j = 0, n = nums.length;
-        for( int i = 0; i < n ; i++){
-            if(nums[j] != nums[i]){
-                j++;
-                if ( j != i )
-                {
-                    nums[j] = nums[i];
-                }
+
+//        int j = 0, n = nums.length;
+//        for( int i = 1; i < n ; i++){
+//            if(nums[j] != nums[i]){
+//                j++;
+//                if ( j != i )
+//                {
+//                    nums[j] = nums[i];
+//                }
+//            }
+//        }
+//        return j;
+        int i = 0;
+        for (int val : nums){
+            if (i < 1 || val>nums[i-1]){
+                nums[i++] = val;
             }
         }
-        return j;
+        return i;
+    }
+
+    public static void main(String[] args) {
+        removeDuplicates(new int[]{1,1,2});
     }
 }
