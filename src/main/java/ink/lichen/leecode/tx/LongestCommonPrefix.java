4a4e46e0b4d5fafd85e222d961a4a5e013083f23
lichen <lichen@daojia-inc.com>
2018-11-13 19:42:34 +0800

    add code

--- src/main/java/ink/lichen/leecode/tx/LongestCommonPrefix.java
+++ src/main/java/ink/lichen/leecode/tx/LongestCommonPrefix.java
@@ -24,12 +24,19 @@ public class LongestCommonPrefix {
             char val = strs[0].charAt(i);
             for (int j = 1 ; j < n; j++ ){
                 if(strs[j].charAt(i) != val){
-                    break;
+                    return strs[0].substring(0,i);
                 }
             }
-
+            i++;
         }
-
+        if (i == min){
+            return strs[0].substring(0,i);
+        }
         return "";
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix l = new LongestCommonPrefix();
+        System.out.println(l.longestCommonPrefix(new String [] {"c","c"}));
     }
 }
