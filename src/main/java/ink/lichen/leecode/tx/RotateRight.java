dfed9b5001f446dbd96c9f2fc31d3e5ae79c6dca
lichen <lichen@daojia-inc.com>
2018-11-13 20:49:33 +0800

    add rotateRight

---
+++ src/main/java/ink/lichen/leecode/tx/RotateRight.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.tx;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-13.
+ */
+public class RotateRight {
+
+    public ListNode rotateRight(ListNode head, int k) {
+
+        return null;
+    }
+}
