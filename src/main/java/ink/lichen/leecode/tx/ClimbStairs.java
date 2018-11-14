c2eb4b3759b2baaa1056434f37f90c0bb64b7782
lichen <lichen@daojia-inc.com>
2018-11-14 15:10:08 +0800

    climbStairs

---
+++ src/main/java/ink/lichen/leecode/tx/ClimbStairs.java
@@ -0,0 +1,21 @@
+package ink.lichen.leecode.tx;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-14.
+ */
+public class ClimbStairs {
+
+    public int climbStairs(int n) {
+//        if (n == 0 || n == 1 || n == 2) {
+//            return n;
+//        }
+//        int[] r = new int[n+1];
+//        r[1] = 1;
+//        r[2] = 2;
+//        for (int i = 3; i <= n; i++) {
+//            r[i] = r[i-1] + r[i-2];
+//        }
+//        return r[n];
+        return 0;
+    }
+}
