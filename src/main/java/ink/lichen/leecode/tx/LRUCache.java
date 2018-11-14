dda182c339d2906df966ff73db7ccae0ce397218
lichen <togetherlichen@gmail.com>
2018-11-14 08:42:28 +0800

    add lRU

--- src/main/java/ink/lichen/leecode/tx/LRUCache.java
+++ src/main/java/ink/lichen/leecode/tx/LRUCache.java
@@ -31,25 +31,61 @@ public class LRUCache {
         this.capacity = capacity;
         head = new Node(0,0);
         tail = new Node(0,0);
+        head.next = tail;
+        tail.pre = head;
         empty = new Node(0,-1);
     }
 
+    public void deleteNode(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
+    public void addToHead(Node node) {
+        node.next = head.next;
+        node.next.pre = node;
+        node.pre = head;
+        head.next = node;
+    }
+
+
     public int get(int key) {
         Node val = map.getOrDefault(key,empty);
-        if (val.value != -1){
-            val.next.pre = val.pre;
-            val.pre.next = val.next;
-            val.next = head.next.next;
-            head.next = val;
-            val.pre = head;
+        System.out.println(val.value);
+        if (val != empty){
+            deleteNode(val);
+
+            addToHead(val);
+
+//            val.next = head.next;
+//            head.next = head.next.pre = val;
+//            val.pre = head;
         }
         return val.value;
     }
 
     public void put(int key, int value) {
-        Node val = map.getOrDefault(key,empty);
-        if (val != empty){
+        Node val = map.get(key);
+        Node newVal = new Node(key,value);
+        if (val != null){
+            deleteNode(val);
 
+            addToHead(newVal);
+            map.put(key,newVal);
+        }else {
+
+            if (map.size() == capacity){
+                map.put(key,newVal);
+                addToHead(newVal);
+
+                Node remove = tail.pre;
+                deleteNode(remove);
+                map.remove(tail.key);
+            }else {
+                map.put(key,newVal);
+                addToHead(newVal);
+            }
         }
+
     }
 }
