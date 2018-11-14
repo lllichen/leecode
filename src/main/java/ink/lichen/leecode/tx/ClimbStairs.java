b4cb82171093d6f1cfe506a195de95d6305f9af5
lichen <lichen@daojia-inc.com>
2018-11-14 17:16:24 +0800

    add maxSubArray

--- src/main/java/ink/lichen/leecode/tx/ClimbStairs.java
+++ src/main/java/ink/lichen/leecode/tx/ClimbStairs.java
@@ -2,20 +2,32 @@ package ink.lichen.leecode.tx;
 
 /**
  * Created by lichen@daojia.com on 2018-11-14.
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ *
  */
 public class ClimbStairs {
 
     public int climbStairs(int n) {
-//        if (n == 0 || n == 1 || n == 2) {
-//            return n;
-//        }
-//        int[] r = new int[n+1];
-//        r[1] = 1;
-//        r[2] = 2;
-//        for (int i = 3; i <= n; i++) {
-//            r[i] = r[i-1] + r[i-2];
-//        }
-//        return r[n];
-        return 0;
+        if (n == 0 || n == 1 || n ==2 ){
+            return n;
+        }
+        int [] r = new int[n+1];
+        r[0] = 0;
+        r[1] = 1;
+        r[2] = 2;
+        int i = 3;
+        while ( i<=n) {
+            r[i] = r[i-1]+r[i-2];
+            i++;
+        }
+        return r[n];
+    }
+
+    public static void main(String[] args) {
+        ClimbStairs climbStairs = new ClimbStairs();
+        System.out.println(climbStairs.climbStairs(4));
     }
 }
---
+++ src/main/java/ink/lichen/leecode/tx/MaxSubArray.java
@@ -0,0 +1,34 @@
+package ink.lichen.leecode.tx;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-14.
+ * 
+ */
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+
+
+//        if (nums.length == 1)
+//            return nums[0];
+//
+//        int max = Integer.MIN_VALUE;
+//        int cur = 0;
+//
+//        for (int i=0; i < nums.length; i++){
+//            cur +=  nums[i];
+//            if (cur < 0){
+//                if( cur > max){
+//                    max = cur;
+//                }
+//                cur = 0;
+//            }else{
+//                if ( cur > max ){
+//                    max = cur;
+//                }
+//            }
+//        }
+//        return max;
+        return 0;
+    }
+}
