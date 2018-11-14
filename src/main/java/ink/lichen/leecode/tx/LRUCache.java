a1392aeec3de8e21f56bf81327c3888849cbfcc5
lichen <lichen@daojia-inc.com>
2018-11-14 14:26:53 +0800

    add lruCache

--- src/main/java/ink/lichen/leecode/tx/LRUCache.java
+++ src/main/java/ink/lichen/leecode/tx/LRUCache.java
@@ -54,7 +54,7 @@ public class LRUCache {
 
     public int get(int key) {
         Node val = map.getOrDefault(key,empty);
-        System.out.println(val.value);
+//        System.out.println(val.value);
         if (val != empty){
             deleteNode(val);
 
@@ -80,12 +80,27 @@ public class LRUCache {
 
                 Node remove = tail.pre;
                 deleteNode(remove);
-                map.remove(tail.key);
+                map.remove(remove.key);
             }else {
                 map.put(key,newVal);
                 addToHead(newVal);
             }
         }
 
+    }
+
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));       // 返回  1
+        cache.put(3, 3);    // 该操作会使得密钥 2 作废
+        System.out.println(cache.get(2));       // 返回 -1 (未找到)
+        cache.put(4, 4);    // 该操作会使得密钥 1 作废
+        System.out.println(cache.get(1));       // 返回 -1 (未找到)
+        System.out.println(cache.get(3));       // 返回  3
+        System.out.println(cache.get(4));       // 返回  4
     }
 }
