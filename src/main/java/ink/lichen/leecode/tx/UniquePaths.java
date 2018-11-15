1aaf4eec4ca51c82078b7236dd6fe3cc50b646de
lichen <lichen@daojia-inc.com>
2018-11-15 16:43:11 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/tx/UniquePaths.java
@@ -0,0 +1,20 @@
+package ink.lichen.leecode.tx;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-15.
+ * 输入: m = 3, n = 2
+ * 输出: 3
+ * 解释:
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向右 -> 向下
+ * 2. 向右 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向右
+ */
+public class UniquePaths {
+
+
+    public int uniquePaths(int m, int n) {
+
+        return 0;
+    }
+}
