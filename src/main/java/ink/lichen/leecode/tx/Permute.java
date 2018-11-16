35af43e2f72e3b523d0e0014db67fbdffe8180b3
lichen <lichen@daojia-inc.com>
2018-11-16 16:17:05 +0800

    add permute

---
+++ src/main/java/ink/lichen/leecode/tx/Permute.java
@@ -0,0 +1,34 @@
+package ink.lichen.leecode.tx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-16.
+ *
+ * 输入: [1,2,3]
+ * 输出:
+ * [
+ *   [1,2,3],
+ *   [1,3,2],
+ *   [2,1,3],
+ *   [2,3,1],
+ *   [3,1,2],
+ *   [3,2,1]
+ * ]
+ */
+public class Permute {
+
+    /**
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+
+
+        return res;
+    }
+}
