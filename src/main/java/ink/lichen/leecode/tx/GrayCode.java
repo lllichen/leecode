e4e9979a874e936c0e40886986f23c1c762b3310
lichen <lichen@daojia-inc.com>
2018-11-16 17:33:26 +0800

    add grayCode

---
+++ src/main/java/ink/lichen/leecode/tx/GrayCode.java
@@ -0,0 +1,33 @@
+package ink.lichen.leecode.tx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-16.
+ *
+ * 格雷编码是一个二进制数字系统，在该系统中，两个连续的数值仅有一个位数的差异。
+ *
+ * 给定一个代表编码总位数的非负整数 n，打印其格雷编码序列。格雷编码序列必须以 0 开头。
+ *
+ * 输入: 2
+ * 输出: [0,1,3,2]
+ * 解释:
+ * 00 - 0
+ * 01 - 1
+ * 11 - 3
+ * 10 - 2
+ *
+ */
+public class GrayCode {
+
+    /**
+     * GrayCode
+     * @param n
+     * @return
+     */
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        return res;
+    }
+}
