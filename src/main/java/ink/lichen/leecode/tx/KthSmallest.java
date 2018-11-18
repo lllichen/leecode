d87fa5fc6a074e038ae910285eee693dbb9b1bb1
lichen <togetherlichen@gmail.com>
2018-11-18 23:13:23 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/tx/KthSmallest.java
@@ -0,0 +1,16 @@
+package ink.lichen.leecode.tx;
+
+import ink.lichen.leecode.support.TreeNode;
+
+
+public class KthSmallest {
+
+
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null){
+            return 0;
+        }
+
+    }
+
+}
