6dfc93502bf8a9ba11879ee6f468ea27a5868507
lichen <togetherlichen@gmail.com>
2018-11-18 22:43:18 +0800

    add FindKthLargest

--- src/main/java/ink/lichen/leecode/tx/FindKthLargest.java
+++ src/main/java/ink/lichen/leecode/tx/FindKthLargest.java
@@ -8,7 +8,7 @@ public class FindKthLargest {
         if (n <2){
             return n;
         }
-
+        quickSelect(nums,left,right,k);
         return nums[k-1];
     }
 
@@ -16,22 +16,64 @@ public class FindKthLargest {
 
     private void quickSelect( int[] nums,int left, int right, int k){
         if (left + CUTOFF <= right){
+            int pivot = median3(nums,left,right);
+            int i = left,j = right-1;
+            for (;;){
+                while (nums[++i] > pivot){};
+                while (nums[--j] < pivot){};
+                if (i < j){
+                    swap(nums,i,j);
+                }else
+                    break;
+            }
+            swap(nums,i,right-1);
+            if ( k <= i){
+                quickSelect(nums,left,i-1,k);
+            }else if (k > i+1)
+                quickSelect(nums,i+1,right,k);
 
         }else{
             insertionSort(nums, left, right);
         }
     }
 
+
     private void insertionSort(int[] nums, int left, int right){
         int j ;
-        for (int i = left;i <= right; i++){
+        for (int i = left+1;i <= right; i++){
             int temp = nums[i];
-            for ( j = left+1; j > left && nums[j-1] < temp ; j--){
+            for ( j = i; j > left && nums[j-1] < temp ; j--){
                 nums[j] = nums[j-1];
             }
             nums[j] = temp;
         }
     }
+
+
+    private int median3(int[] nums, int left,int right){
+
+        int mid = (left + right) >> 1;
+        if (nums[mid] > nums[left] ){
+            swap(nums,mid,left);
+        }
+        if ( nums[right] > nums[left] ){
+            swap(nums,right,left);
+        }
+
+        if (nums[right] > nums[mid] ){
+            swap(nums,right,mid);
+        }
+        swap(nums,mid,right-1);
+        return nums[right-1];
+    }
+
+    private void swap(int[] nums, int left, int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
+
 
 
 }
