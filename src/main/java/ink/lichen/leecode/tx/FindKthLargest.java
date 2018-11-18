46e5820aee8d8db70afbbced6914616bdb7080fe
lichen <togetherlichen@gmail.com>
2018-11-18 22:01:04 +0800

    fix error

--- src/main/java/ink/lichen/leecode/tx/FindKthLargest.java
+++ src/main/java/ink/lichen/leecode/tx/FindKthLargest.java
@@ -1,4 +1,37 @@
 package ink.lichen.leecode.tx;
 
 public class FindKthLargest {
+
+
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length, left = 0, right = n-1;
+        if (n <2){
+            return n;
+        }
+
+        return nums[k-1];
+    }
+
+    private static final int CUTOFF = 10;
+
+    private void quickSelect( int[] nums,int left, int right, int k){
+        if (left + CUTOFF <= right){
+
+        }else{
+            insertionSort(nums,int left, int right);
+        }
+    }
+
+    private void insertionSort(int[] nums, int left, int right){
+        int j ;
+        for (int i = left;i <= right; i++){
+            int temp = nums[i];
+            for ( j = left+1; j > left && nums[j-1] < temp ; j--){
+                nums[j] = nums[j-1];
+            }
+            nums[j] = temp;
+        }
+    }
+
+
 }
