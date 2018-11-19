90ebb2ab9ae18ab60d69750491a7b362e2131452
lichen <lichen@daojia-inc.com>
2018-11-19 16:12:46 +0800

    add new

--- src/main/java/ink/lichen/leecode/tx/LowestCommonAncestor2.java
+++ src/main/java/ink/lichen/leecode/tx/LowestCommonAncestor2.java
@@ -17,7 +17,7 @@ import ink.lichen.leecode.support.TreeNode;
  *
  * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
  * 输出: 3
- * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。~
  */
 public class LowestCommonAncestor2 {
 
