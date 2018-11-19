8ea6e800d92d528389e020707eb7f138b959421f
lichen <lichen@daojia-inc.com>
2018-11-19 20:15:27 +0800

    add two number

---
+++ src/main/java/ink/lichen/leecode/tx/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package ink.lichen.leecode.tx;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-19.
+ */
+public class AddTwoNumbers {
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+}
