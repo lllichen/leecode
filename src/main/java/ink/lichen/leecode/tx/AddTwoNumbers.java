5e6979aa38c06ab92b942b0ceea389eab7023ef9
lichen <lichen@daojia-inc.com>
2018-11-19 20:16:30 +0800

    addTwiNumbers

--- src/main/java/ink/lichen/leecode/tx/AddTwoNumbers.java
+++ src/main/java/ink/lichen/leecode/tx/AddTwoNumbers.java
@@ -4,6 +4,11 @@ import ink.lichen.leecode.support.ListNode;
 
 /**
  * Created by lichen@daojia.com on 2018-11-19.
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
  */
 public class AddTwoNumbers {
 
