76c1440f037bca56acfcfb72d13d488acb9d51e9
lichen <lichen@daojia-inc.com>
2018-11-19 20:09:11 +0800

    add IsPowerOfTwo

---
+++ src/main/java/ink/lichen/leecode/tx/IsPowerOfTwo.java
@@ -0,0 +1,17 @@
+package ink.lichen.leecode.tx;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-19.
+ */
+public class IsPowerOfTwo {
+
+    public boolean isPowerOfTwo(int n) {
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1>>2);
+    }
+}
