6b30e5d5cf54b0560eba381a4a9c9127c53d797e
lichen <lichen@daojia-inc.com>
2018-11-19 17:14:15 +0800

    add MajorityElement

---
+++ src/main/java/ink/lichen/leecode/tx/MajorityElement.java
@@ -0,0 +1,19 @@
+package ink.lichen.leecode.tx;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-19.
+ *
+ * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+ *
+ * 输入: [3,2,3]
+ * 输出: 3
+ */
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+
+        return 0;
+    }
+}
