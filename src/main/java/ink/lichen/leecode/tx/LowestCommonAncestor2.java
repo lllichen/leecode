f5959605088f046789c5d2812917e28456362226
lichen <lichen@daojia-inc.com>
2018-11-19 16:11:21 +0800

    add new solution

---
+++ src/main/java/ink/lichen/leecode/tx/LowestCommonAncestor2.java
@@ -0,0 +1,28 @@
+package ink.lichen.leecode.tx;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-19.
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ *
+ *         _______3______
+ *        /              \
+ *     ___5__          ___1__
+ *    /      \        /      \
+ *    6      _2       0       8
+ *          /  \
+ *          7   4
+ * 示例 1:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ */
+public class LowestCommonAncestor2 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+}
