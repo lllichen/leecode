bcf2866d0c743e33b2fdff935812372dced7710c
lichen <lichen@daojia-inc.com>
2018-11-19 08:43:16 +0800

    add graycode

--- src/main/java/ink/lichen/leecode/tx/GrayCode.java
+++ src/main/java/ink/lichen/leecode/tx/GrayCode.java
@@ -47,4 +47,10 @@ public class GrayCode {
     public void backtrack(List<Integer> res, int n){
 
     }
+
+    public static void main(String[] args) {
+        System.out.println(1<<2);
+        System.out.println(1>>1);
+        System.out.println(1 ^ 1>>1);
+    }
 }
