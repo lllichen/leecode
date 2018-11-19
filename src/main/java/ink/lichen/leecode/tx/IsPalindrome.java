d2900f4bfe8e83ac24cee2017295438267d110a3
lichen <lichen@daojia-inc.com>
2018-11-19 16:41:26 +0800

    add isPalindrome

--- src/main/java/ink/lichen/leecode/tx/IsPalindrome.java
+++ src/main/java/ink/lichen/leecode/tx/IsPalindrome.java
@@ -1,5 +1,7 @@
 package ink.lichen.leecode.tx;
 
+import java.util.Stack;
+
 /**
  * Created by lichen@daojia.com on 2018-11-19.
  */
@@ -7,7 +9,16 @@ public class IsPalindrome {
 
 
     public boolean isPalindrome(int x) {
+        if (x < 0){
+            return false;
+        }
+        if (x < 10){
+            return true;
+        }
 
-
+        int a = x,h=1;
+        while (a/10 >= 10){
+            h = h/10;
+        }
     }
 }
