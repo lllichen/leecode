c0784779a21de6350f0652fbeced33c30c8da175
lichen <lichen@daojia-inc.com>
2018-11-19 09:14:10 +0800

    add maxDepth

--- src/main/java/ink/lichen/leecode/tx/MaxDepth.java
+++ src/main/java/ink/lichen/leecode/tx/MaxDepth.java
@@ -1,10 +1,20 @@
 package ink.lichen.leecode.tx;
 
+import ink.lichen.leecode.support.TreeNode;
+
 /**
  * Created by lichen@daojia.com on 2018-11-19.
  */
 public class MaxDepth {
 
 
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left)+1;
+        int rightDepth = maxDepth(root.right) +1;
+        return leftDepth > rightDepth ? leftDepth:rightDepth;
 
+    }
 }
