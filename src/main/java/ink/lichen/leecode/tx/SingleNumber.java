503bc669dc62a98aeddbb9204675da5f4989025e
lichen <lichen@daojia-inc.com>
2018-11-19 17:04:32 +0800

    add info

--- src/main/java/ink/lichen/leecode/tx/SingleNumber.java
+++ src/main/java/ink/lichen/leecode/tx/SingleNumber.java
@@ -2,6 +2,10 @@ package ink.lichen.leecode.tx;
 
 /**
  * Created by lichen@daojia.com on 2018-11-19.
+ * 输入: [2,2,1]
+ * 输出: 1
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
  */
 public class SingleNumber {
 
