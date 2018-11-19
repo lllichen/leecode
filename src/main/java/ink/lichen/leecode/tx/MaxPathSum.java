0652c7500ab204b15eaf6eb12fa0ad01c9bf2e71
lichen <lichen@daojia-inc.com>
2018-11-19 09:45:13 +0800

    add MaxPathSum

---
+++ src/main/java/ink/lichen/leecode/tx/MaxPathSum.java
@@ -0,0 +1,24 @@
+package ink.lichen.leecode.tx;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-19.
+ *
+ * 输入: [1,2,3]
+ *
+ *        1
+ *       / \
+ *      2   3
+ *
+ * 输出: 6
+ *
+ */
+public class MaxPathSum {
+
+
+    public int maxPathSum(TreeNode root) {
+
+        return 0;
+    }
+}
