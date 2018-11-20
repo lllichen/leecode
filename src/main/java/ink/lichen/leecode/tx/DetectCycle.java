966f89302bf075b4ec338f3baf9a9b2ec870c635
lichen <lichen@daojia-inc.com>
2018-11-20 11:04:45 +0800

    add DetectCycle

---
+++ src/main/java/ink/lichen/leecode/tx/DetectCycle.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.tx;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-20.
+ */
+public class DetectCycle {
+
+    public ListNode detectCycle(ListNode head) {
+
+        return null;
+    }
+}
