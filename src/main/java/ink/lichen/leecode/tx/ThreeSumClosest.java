56ed9d773e012056f9f30985f7eb538460b677f4
lichen <lichen@daojia-inc.com>
2018-11-20 12:34:53 +0800

    add threeSumClosest

---
+++ src/main/java/ink/lichen/leecode/tx/ThreeSumClosest.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode.tx;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-20.
+ */
+public class ThreeSumClosest {
+
+
+    public int threeSumClosest(int[] nums, int target) {
+
+        return 0;
+    }
+}
