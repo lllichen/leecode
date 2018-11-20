803c3d453e0e979370c0bfc2a218673faa04c936
lichen <lichen@daojia-inc.com>
2018-11-20 16:22:33 +0800

    add reverseString

--- src/main/java/ink/lichen/leecode/tx/ReverseWords.java
+++ src/main/java/ink/lichen/leecode/tx/ReverseWords.java
@@ -6,7 +6,28 @@ package ink.lichen.leecode.tx;
 public class ReverseWords {
 
     public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        int n = s.length();
+        int mark = 0;
+        int cur = 0;
+        while (cur < n){
+            if (cur == ' '){
+                mark = cur;
+            }
+            reverseString()
+        }
 
         return "";
+    }
+
+    public void reverseString(char[] chars,int left, int right) {
+        for (int i = 0,j = chars.length-1 ; i < chars.length; i++,j--){
+            if (i >= j ){
+                break;
+            }
+            char tmp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = tmp;
+        }
     }
 }
