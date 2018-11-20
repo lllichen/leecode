8a2f37c5e8664e272f7bb2c7c481d302b1be5542
lichen <lichen@daojia-inc.com>
2018-11-20 10:25:28 +0800

    add rotateRight

--- src/main/java/ink/lichen/leecode/tx/RotateRight.java
+++ src/main/java/ink/lichen/leecode/tx/RotateRight.java
@@ -9,23 +9,53 @@ import ink.lichen.leecode.support.ListNode;
  * 解释:
  * 向右旋转 1 步: 5->1->2->3->4->NULL
  * 向右旋转 2 步: 4->5->1->2->3->NULL
+ *
+ * [1,2]
+ * 1
+ * [1]
+ * 1
  */
 public class RotateRight {
 
     public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k == 0){
+            return head;
+        }
         int n = 0;
         ListNode curNode = head;
         ListNode markHead = head;
+        ListNode pre = head;
         while (curNode != null){
-            n++;
             curNode = curNode.next;
+            n++;
+            if (pre.next != null){
+                pre = curNode;
+            }
         }
-        curNode.next = head;
-        int val = k % n ;
-        while (val >= 0){
+        pre.next = head;
+//        System.out.println(pre.val);
+        int val =n - k % n ;
+
+//      [1,2]
+//      1
+        pre = markHead;
+        while (val > 0){
+//            System.out.println(val);
             markHead = markHead.next;
+            if (val > 1){
+                pre = pre.next;
+            }
             val--;
+
         }
-        return null;
+        pre.next = null;
+        return markHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        RotateRight rotateRight = new RotateRight();
+        rotateRight.rotateRight(head,1);
     }
 }
