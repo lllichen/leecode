d24f198e231da7c787f5c70cc57458ce627971f7
lichen <lichen@daojia-inc.com>
2018-11-20 11:19:23 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/tx/GetIntersectionNode.java
@@ -0,0 +1,26 @@
+package ink.lichen.leecode.tx;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-20.
+ *
+ * 例如，下面的两个链表：
+ *
+ * A:          a1 → a2
+ *                    ↘
+ *                      c1 → c2 → c3
+ *                    ↗
+ * B:     b1 → b2 → b3
+ * 在节点 c1 开始相交。
+ *
+ */
+public class GetIntersectionNode {
+
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+
+
+    }
+}
