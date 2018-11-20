9efa8523abe1565251d64522dd20c98809ab3b7c
lichen <lichen@daojia-inc.com>
2018-11-20 16:57:53 +0800

    add ProductExceptSelf

--- src/main/java/ink/lichen/leecode/tx/ProductExceptSelf.java
+++ src/main/java/ink/lichen/leecode/tx/ProductExceptSelf.java
@@ -2,12 +2,32 @@ package ink.lichen.leecode.tx;
 
 /**
  * Created by lichen@daojia.com on 2018-11-20.
+ *
+ * 输入: [1,2,3,4]
+ * 输出: [24,12,8,6]
+ *
  */
 public class ProductExceptSelf {
 
 
-    public int[] productExceptSelf(int[] nums) {
-
-        return new int[10];
+    public int[] productExceptSelfRev(int[] nums) {
+        if(nums.length <= 1){
+            return nums;
+        }
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        int left = 1, right = 1;
+        // 计算每个点左边的乘积
+        for(int i = 1; i < nums.length; i++){
+            left = left * nums[i - 1];
+            res[i] = left;
+        }
+        // 计算每个点右边的乘积
+        for(int i = nums.length - 2; i >= 0; i--){
+            right = right * nums[i + 1];
+            res[i] = right * res[i];
+        }
+        return res;
     }
+
 }
