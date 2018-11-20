93786c1cd0d92b1ddaedbe0857459a540e07a5cd
lichen <lichen@daojia-inc.com>
2018-11-20 15:41:34 +0800

    add Multiply

--- src/main/java/ink/lichen/leecode/tx/Multiply.java
+++ src/main/java/ink/lichen/leecode/tx/Multiply.java
@@ -4,4 +4,9 @@ package ink.lichen.leecode.tx;
  * Created by lichen@daojia.com on 2018-11-20.
  */
 public class Multiply {
+
+    public String multiply(String num1, String num2) {
+
+        return "";
+    }
 }
