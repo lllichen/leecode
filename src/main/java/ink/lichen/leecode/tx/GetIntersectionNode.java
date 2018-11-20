a2ff2581e4df31772bc490dcda4f00bc6142e0b6
lichen <lichen@daojia-inc.com>
2018-11-20 11:19:47 +0800

    add info

--- src/main/java/ink/lichen/leecode/tx/GetIntersectionNode.java
+++ src/main/java/ink/lichen/leecode/tx/GetIntersectionNode.java
@@ -14,6 +14,12 @@ import ink.lichen.leecode.support.ListNode;
  * B:     b1 → b2 → b3
  * 在节点 c1 开始相交。
  *
+ * 如果两个链表没有交点，返回 null.
+ * 在返回结果后，两个链表仍须保持原有的结构。
+ * 可假定整个链表结构中没有循环。
+ * 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+ *
+ *
  */
 public class GetIntersectionNode {
 
