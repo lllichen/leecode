8a1f93aac72993230db13b1bad2d1a674c3a7a8e
lichen <lichen@daojia-inc.com>
2018-11-22 17:19:03 +0800

    add  copyRandomList

--- src/main/java/ink/lichen/leecode/hard/list/CopyRandomList.java
+++ src/main/java/ink/lichen/leecode/hard/list/CopyRandomList.java
@@ -4,12 +4,27 @@ import ink.lichen.leecode.support.RandomListNode;
 
 /**
  * Created by lichen@daojia.com on 2018-11-22.
+ *
+ *给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+ *
+ *要求返回这个链表的深度拷贝
  */
 public class CopyRandomList {
 
 
     public RandomListNode copyRandomList(RandomListNode head) {
 
-        return null;
+        if (head == null){
+            return null;
+        }
+        RandomListNode res = new RandomListNode(head.label);
+        res.random =
+        head = head.next;
+        while (head != null){
+            res.next = new RandomListNode(head.label);
+            res.next.random = head.random;
+        }
+
+        return res;
     }
 }
