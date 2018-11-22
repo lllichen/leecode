1978cb5cad12dcf282897b9686ffe92d6a1670d0
lichen <lichen@daojia-inc.com>
2018-11-22 17:09:52 +0800

    add CopyRandomList

---
+++ src/main/java/ink/lichen/leecode/hard/list/CopyRandomList.java
@@ -0,0 +1,15 @@
+package ink.lichen.leecode.hard.list;
+
+import ink.lichen.leecode.support.RandomListNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-22.
+ */
+public class CopyRandomList {
+
+
+    public RandomListNode copyRandomList(RandomListNode head) {
+
+        return null;
+    }
+}
