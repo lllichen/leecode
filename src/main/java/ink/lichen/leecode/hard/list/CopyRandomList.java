d8952e903abb725acd8b9d492887006bad94c3ca
lichen <lichen@daojia-inc.com>
2018-11-22 17:42:28 +0800

    add CopyRandomList

--- src/main/java/ink/lichen/leecode/hard/list/CopyRandomList.java
+++ src/main/java/ink/lichen/leecode/hard/list/CopyRandomList.java
@@ -2,6 +2,12 @@ package ink.lichen.leecode.hard.list;
 
 import ink.lichen.leecode.support.RandomListNode;
 
+import javax.xml.soap.Node;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by lichen@daojia.com on 2018-11-22.
  *
@@ -17,14 +23,27 @@ public class CopyRandomList {
         if (head == null){
             return null;
         }
-        RandomListNode res = new RandomListNode(head.label);
-        res.random =
-        head = head.next;
+        Map<RandomListNode,RandomListNode> oldNewMap = new HashMap<RandomListNode,RandomListNode>();
+
+        RandomListNode dummy = new RandomListNode(0);
+
+        RandomListNode cur = dummy;
+
         while (head != null){
-            res.next = new RandomListNode(head.label);
-            res.next.random = head.random;
+            RandomListNode newNode = new RandomListNode(head.label);
+            oldNewMap.put(head,newNode);
+            head = head.next;
+            cur.next = newNode;
+            cur = cur.next;
         }
 
-        return res;
+        RandomListNode newHead = dummy.next;
+        while (head != null){
+            newHead.random = oldNewMap.get(head.random);
+            head = head.next;
+            newHead = newHead.next;
+        }
+
+        return dummy.next;
     }
 }
