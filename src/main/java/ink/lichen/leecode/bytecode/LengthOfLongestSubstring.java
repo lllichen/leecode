cb4ff24bf9c22705128af0268a251412403872b7
lichen <lichen@daojia-inc.com>
2018-11-23 18:43:44 +0800

    add LengthOfLongestSubstring

---
+++ src/main/java/ink/lichen/leecode/bytecode/LengthOfLongestSubstring.java
@@ -0,0 +1,24 @@
+package ink.lichen.leecode.bytecode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-23.
+ */
+public class LengthOfLongestSubstring {
+
+
+
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128];
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+
+}
