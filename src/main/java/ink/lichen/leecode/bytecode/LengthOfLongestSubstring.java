15d43a16e56350fd1eb98d332f10e359197b038e
lichen <lichen@daojia-inc.com>
2018-11-23 18:48:39 +0800

    add info

--- src/main/java/ink/lichen/leecode/bytecode/LengthOfLongestSubstring.java
+++ src/main/java/ink/lichen/leecode/bytecode/LengthOfLongestSubstring.java
@@ -22,10 +22,11 @@ public class LengthOfLongestSubstring {
     }
 
     public static void main(String[] args) {
-        System.out.println((int)'a');
-        System.out.println((int)'z');
         System.out.println((int)'A');
         System.out.println((int)'Z');
+
+        System.out.println((int)'a');
+        System.out.println((int)'z');
     }
 
 }
