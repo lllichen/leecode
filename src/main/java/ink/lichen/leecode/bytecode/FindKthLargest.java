5aa68b29c043e8556fa7cdf787149fe2af8969be
lichen <lichen@daojia-inc.com>
2018-11-26 11:50:00 +0800

    add FindKthLargest

---
+++ src/main/java/ink/lichen/leecode/bytecode/FindKthLargest.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.bytecode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-26.
+ */
+public class FindKthLargest {
+
+    public int findKthLargest(int[] nums, int k) {
+
+        return nums[k-1];
+    }
+}
