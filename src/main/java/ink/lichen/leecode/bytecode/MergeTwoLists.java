8a7ef4bc46769670760849ce8836f1803caeb2f1
lichen <togetherlichen@gmail.com>
2018-11-26 23:00:56 +0800

    fix code

---
+++ src/main/java/ink/lichen/leecode/bytecode/MergeTwoLists.java
@@ -0,0 +1,34 @@
+package ink.lichen.leecode.bytecode;
+
+import ink.lichen.leecode.support.ListNode;
+
+public class MergeTwoLists {
+
+
+
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode merge = new ListNode(0);
+        ListNode cur = merge;
+
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                cur.next = l1;
+                l1 =l1.next;
+            }else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+
+        if (l1  != null){
+            cur.next = l1;
+        }else {
+            cur.next = l2;
+        }
+        return merge.next;
+    }
+
+}
--- src/main/java/ink/lichen/leecode/bytecode/Trap.java
+++ src/main/java/ink/lichen/leecode/bytecode/Trap.java
@@ -4,8 +4,28 @@ public class Trap {
 
 
     public int trap(int[] height) {
+        int n = height.length;
+        if (n <= 1){
+            return n;
+        }
+        int left = 0;
+        int right = n -1;
+        int leftMax= 0;
+        int rightMax = 0;
+        int res = 0;
+        while (left < right){
+            if (height[left] < height[right]){
+                leftMax = Math.max(height[left],leftMax);
+                res += leftMax - height[left];
+                left++;
+            }else {
+                rightMax = Math.max(height[right],rightMax);
+                res += rightMax - height[right];
+                right--;
+            }
+        }
 
-        return 0;
+        return res;
     }
 
 
