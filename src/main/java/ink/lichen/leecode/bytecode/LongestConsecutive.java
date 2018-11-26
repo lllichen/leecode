8376b05dd7bbb71968babe0f240ee48bc6b13fac
lichen <lichen@daojia-inc.com>
2018-11-26 11:52:30 +0800

    add LongestConsecutive comment

--- src/main/java/ink/lichen/leecode/bytecode/LongestConsecutive.java
+++ src/main/java/ink/lichen/leecode/bytecode/LongestConsecutive.java
@@ -2,6 +2,8 @@ package ink.lichen.leecode.bytecode;
 
 /**
  * Created by lichen@daojia.com on 2018-11-26.
+ * 给定一个未排序的整数数组，找出最长连续序列的长度。
+ * 要求算法的时间复杂度为 O(n)。
  */
 public class LongestConsecutive {
 
