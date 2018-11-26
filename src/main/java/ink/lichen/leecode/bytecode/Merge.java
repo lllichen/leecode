685a716d7114482cfcc5f10cfe139d5c8dd61954
lichen <lichen@daojia-inc.com>
2018-11-26 18:42:11 +0800

    add merge code

--- src/main/java/ink/lichen/leecode/bytecode/Merge.java
+++ src/main/java/ink/lichen/leecode/bytecode/Merge.java
@@ -1,7 +1,17 @@
 package ink.lichen.leecode.bytecode;
 
+import ink.lichen.leecode.support.Interval;
+
+import java.util.List;
+
 /**
  * Created by lichen@daojia.com on 2018-11-26.
  */
 public class Merge {
+
+
+    public List<Interval> merge(List<Interval> intervals) {
+
+        return null;
+    }
 }
