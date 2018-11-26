704f5d4038c37c530f2aa1eb36575bbcc43654e2
lichen <lichen@daojia-inc.com>
2018-11-26 10:27:35 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/bytecode/FindLengthOfLCIS.java
@@ -0,0 +1,20 @@
+package ink.lichen.leecode.bytecode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-26.
+ *
+ * 输入: [1,3,5,4,7]
+ * 输出: 3
+ * 解释: 最长连续递增序列是 [1,3,5], 长度为3。
+ * 尽管 [1,3,5,7] 也是升序的子序列, 但它不是连续的，因为5和7在原数组里被4隔开。
+ *
+ */
+public class FindLengthOfLCIS {
+
+
+    public int findLengthOfLCIS(int[] nums) {
+
+        return 1;
+
+    }
+}
