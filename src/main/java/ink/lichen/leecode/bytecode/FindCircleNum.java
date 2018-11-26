e0f3127cb7212ddf97073398cfc02ec32c43a645
lichen <lichen@daojia-inc.com>
2018-11-26 18:00:52 +0800

    add findCircleNum

---
+++ src/main/java/ink/lichen/leecode/bytecode/FindCircleNum.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode.bytecode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-26.
+ */
+public class FindCircleNum {
+
+
+    public int findCircleNum(int[][] M) {
+
+        return 0;
+    }
+}
--- src/main/java/ink/lichen/leecode/bytecode/GetPermutation.java
+++ src/main/java/ink/lichen/leecode/bytecode/GetPermutation.java
@@ -13,12 +13,14 @@ import java.util.List;
 
      按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
 
-     "123"
-     "132"
-     "213"
-     "231"
-     "312"
-     "321"
+ *   "123"
+ *   "132"
+ *
+ *   "213"
+ *   "231"
+ *
+ *   "312"
+ *   "321"
  *
  * 输入: n = 3, k = 3
  * 输出: "213"
@@ -30,15 +32,30 @@ public class GetPermutation {
     private List<Integer> k = new ArrayList<>();
 
     public String getPermutation(int n, int k) {
-        int sum = 1;
-        for (int i = 1 ; i < n ; i++) {
-            sum *= i;
+        int[] num = new int[n];
+        int perNumCount = 1;
+
+        for(int i = 0; i < n; i++) {
+            num[i] = i+1;
+            perNumCount *= i + 1;
         }
+        k--;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            perNumCount = perNumCount / (n - i);
+            int choosed = k / perNumCount;
+            sb.append(String.valueOf(num[choosed]));
+            for(int j = choosed; j < n - i - 1; j++) {
+                num[j] = num[j+1];
+            }
+            k = k % perNumCount;
+        }
+        return sb.toString();
+    }
 
-        int val = k / sum;
-        int t = k % sum;
-
-        return "";
+    public static void main(String[] args) {
+        GetPermutation getPermutation = new GetPermutation();
+        System.out.println(getPermutation.getPermutation(3,3));
     }
 
 }
