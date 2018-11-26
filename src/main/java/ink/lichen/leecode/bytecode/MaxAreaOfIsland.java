dc80ca7dc4a907e527bdf35b61cdc5b5e0f6c8b7
lichen <lichen@daojia-inc.com>
2018-11-26 10:23:02 +0800

    add MaxAreaOfIsland

--- src/main/java/ink/lichen/leecode/bytecode/MaxAreaOfIsland.java
+++ src/main/java/ink/lichen/leecode/bytecode/MaxAreaOfIsland.java
@@ -18,16 +18,19 @@ public class MaxAreaOfIsland {
 
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
+        int max = 0;
         if (n == 0){
             return 0;
         }
         int m = grid[0].length;
         for (int i = 0 ; i < n; i++){
             for (int j = 0 ; j <m; j++ ) {
-
                 if (grid[i][j] == 1){
                     int temp = 0;
-                    dfs(grid,temp,i,j);
+                    temp = dfs(grid,temp,i,j);
+                    if (max < temp){
+                        max = temp;
+                    }
                 }
 
             }
@@ -35,29 +38,28 @@ public class MaxAreaOfIsland {
         return max;
     }
 
-    private static int max = 0;
-
-    public void dfs(int[][] grid, int temp,int i, int j){
+    public int dfs(int[][] grid, int temp,int i, int j){
         if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length ){
             if (grid[i][j] == 1){
                 temp++;
-                if(max <temp){
-                    max = temp;
-                }
                 grid[i][j] = 0;
 
-              dfs(grid,temp,i-1,j);
-              dfs(grid,temp,i+1,j);
-              dfs(grid,temp,i,j-1);
-              dfs(grid,temp,i,j+1);
+              temp = dfs(grid,temp,i-1,j);
+                temp = dfs(grid,temp,i+1,j);
+                temp =dfs(grid,temp,i,j-1);
+                temp =dfs(grid,temp,i,j+1);
             }
         }
-        return;
+        return temp;
     }
 
     public static void main(String[] args) {
         MaxAreaOfIsland maxAreaOfIsland = new MaxAreaOfIsland();
-        int[][] grinds = new int[][]{};
+        int[][] grinds = new int[][]{
+                {1,1,0,1,1},
+                {1,0,0,0,0},
+                {0,0,0,0,1},
+                {1,1,0,1,1}};
         System.out.println(maxAreaOfIsland.maxAreaOfIsland(grinds));
     }
 
