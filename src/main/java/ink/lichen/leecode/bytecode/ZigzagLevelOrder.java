a175a810fd618cc6f5c2337d1c5b3dacc98ff84c
lichen <lichen@daojia-inc.com>
2018-11-27 18:25:51 +0800

    add ZigzagLevelOrder

--- src/main/java/ink/lichen/leecode/bytecode/ZigzagLevelOrder.java
+++ src/main/java/ink/lichen/leecode/bytecode/ZigzagLevelOrder.java
@@ -1,8 +1,12 @@
 package ink.lichen.leecode.bytecode;
 
+import ink.lichen.leecode.support.ListNode;
 import ink.lichen.leecode.support.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by lichen@daojia.com on 2018-11-27.
@@ -28,7 +32,27 @@ public class ZigzagLevelOrder {
 
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
 
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int size = queue.size();
+        while (size > 0){
+            TreeNode node = queue.poll();
+            if (node.right != null){
+                queue.add(node.right);
+            }
+            if (node.left != null){
+                queue.add(node.left);
+            }
+            size--;
+            if (size == 0){
+                size = queue.size();
+            }
+        }
         return null;
     }
 }
