307eb8ecdfcdabc081430ecce68b89ba06e9f9f1
lichen <lichen@daojia-inc.com>
2018-11-27 18:14:40 +0800

    add ZigzagLevelOrder

--- src/main/java/ink/lichen/leecode/bytecode/LowestCommonAncestor.java
+++ src/main/java/ink/lichen/leecode/bytecode/LowestCommonAncestor.java
@@ -23,6 +23,12 @@ public class LowestCommonAncestor {
 
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null){
+            System.out.println("null");
+        }else {
+            System.out.println(root.val);
+        }
+
         if (root == null || p == root || q == root)
             return root;
 
@@ -37,18 +43,39 @@ public class LowestCommonAncestor {
     }
 
     public static void main(String[] args) {
+//        TreeNode left5 = new TreeNode(5);
+//        TreeNode right1 = new TreeNode(1);
+//        TreeNode root = new TreeNode(3);
+//        root.left = left5;
+//        root.left.left = new TreeNode(6);
+//        root.left.right = new TreeNode(2);
+//        root.left.right.left = new TreeNode(7);
+//        root.left.right.right = new TreeNode(4);
+//
+//        root.right = new TreeNode(1);
+//        root.right.left = new TreeNode(0);
+//        root.right.right = new TreeNode(8);
+
+
+        TreeNode root = new TreeNode(3);
+
         TreeNode left5 = new TreeNode(5);
         TreeNode right1 = new TreeNode(1);
-        TreeNode root = new TreeNode(3);
         root.left = left5;
+        root.right = right1;
+
         root.left.left = new TreeNode(6);
         root.left.right = new TreeNode(2);
-        root.left.right.left = new TreeNode(7);
-        root.left.right.right = new TreeNode(4);
 
-        root.right = new TreeNode(1);
+        TreeNode left7 = new TreeNode(7);
+        TreeNode right4 = new TreeNode(4);
+        root.left.right.left = left7;
+        root.left.right.right = right4;
+
+//        root.right = new TreeNode(1);
         root.right.left = new TreeNode(0);
         root.right.right = new TreeNode(8);
+
 
         LowestCommonAncestor lowestCommonAncestor = new LowestCommonAncestor();
         lowestCommonAncestor.lowestCommonAncestor(root,left5,right1);
---
+++ src/main/java/ink/lichen/leecode/bytecode/ZigzagLevelOrder.java
@@ -0,0 +1,34 @@
+package ink.lichen.leecode.bytecode;
+
+import ink.lichen.leecode.support.TreeNode;
+
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-27.
+ *
+ * 给定一个二叉树，返回其节点值的锯齿形层次遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ *   [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ *   ]
+ */
+public class ZigzagLevelOrder {
+
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        return null;
+    }
+}
