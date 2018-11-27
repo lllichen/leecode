7956f5280a7e1afa019503307526e7dc62247b9d
lichen <lichen@daojia-inc.com>
2018-11-27 14:22:25 +0800

    add GetInter

---
+++ src/main/java/ink/lichen/leecode/bytecode/GetIntersectionNode.java
@@ -0,0 +1,15 @@
+package ink.lichen.leecode.bytecode;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-27.
+ */
+public class GetIntersectionNode {
+
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        return null;
+    }
+}
