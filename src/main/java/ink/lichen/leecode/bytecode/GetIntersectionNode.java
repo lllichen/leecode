4cd5463df5779b175eb06a32b5fb7cf090f2a4fd
lichen <lichen@daojia-inc.com>
2018-11-27 16:32:02 +0800

    add info

--- src/main/java/ink/lichen/leecode/bytecode/GetIntersectionNode.java
+++ src/main/java/ink/lichen/leecode/bytecode/GetIntersectionNode.java
@@ -4,6 +4,17 @@ import ink.lichen.leecode.support.ListNode;
 
 /**
  * Created by lichen@daojia.com on 2018-11-27.
+ * A:          a1 → a2
+ *                    ↘
+ *                      c1 → c2 → c3
+ *                    ↗
+ * B:     b1 → b2 → b3
+ *
+ * 如果两个链表没有交点，返回 null.
+ * 在返回结果后，两个链表仍须保持原有的结构。
+ * 可假定整个链表结构中没有循环。
+ * 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+ *
  */
 public class GetIntersectionNode {
 
