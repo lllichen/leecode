efb73deeafda389181156650e4e97678f38fa2ba
lichen <togetherlichen@gmail.com>
2018-11-27 23:33:00 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/bytecode/MaxProfit.java
@@ -0,0 +1,10 @@
+package ink.lichen.leecode.bytecode;
+
+public class MaxProfit {
+
+    public int maxProfit(int[] prices) {
+
+
+        return 0;
+    }
+}
--- src/main/java/ink/lichen/leecode/bytecode/ZigzagLevelOrder.java
+++ src/main/java/ink/lichen/leecode/bytecode/ZigzagLevelOrder.java
@@ -3,10 +3,7 @@ package ink.lichen.leecode.bytecode;
 import ink.lichen.leecode.support.ListNode;
 import ink.lichen.leecode.support.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * Created by lichen@daojia.com on 2018-11-27.
@@ -37,22 +34,29 @@ public class ZigzagLevelOrder {
             return res;
         }
 
-        Queue<TreeNode> queue = new ArrayDeque<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        int size = queue.size();
-        while (size > 0){
-            TreeNode node = queue.poll();
-            if (node.right != null){
-                queue.add(node.right);
+        boolean flag = true;
+        while (!queue.isEmpty()){
+            List<Integer> list = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0 ; i < size; i++){
+                TreeNode node = queue.poll();
+                if (flag){
+                    list.add(node.val);
+                }else {
+                    list.add(0,node.val);
+                }
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
             }
-            if (node.left != null){
-                queue.add(node.left);
-            }
-            size--;
-            if (size == 0){
-                size = queue.size();
-            }
+            res.add(list);
+            flag = !flag;
         }
-        return null;
+        return res;
     }
 }
