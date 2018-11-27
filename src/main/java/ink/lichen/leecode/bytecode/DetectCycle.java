fff945ea671f9011fef7f7e07bf735d9cafb9980
lichen <lichen@daojia-inc.com>
2018-11-27 16:30:32 +0800

    add DetectCycle

--- src/main/java/ink/lichen/leecode/bytecode/DetectCycle.java
+++ src/main/java/ink/lichen/leecode/bytecode/DetectCycle.java
@@ -9,21 +9,18 @@ import ink.lichen.leecode.support.ListNode;
 public class DetectCycle {
 
     public ListNode detectCycle(ListNode head) {
-
-        ListNode slow = head;
         ListNode fast = head;
-
-        while (fast!=null && fast.next!=null){
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
             fast = fast.next.next;
             slow = slow.next;
-
-            if (fast == slow){
+            while (slow == fast){
                 ListNode slow2 = head;
-                while (slow2 != slow){
+                while (slow != slow2){
                     slow = slow.next;
                     slow2 = slow2.next;
                 }
-                return slow;
+                return  slow;
             }
         }
         return null;
