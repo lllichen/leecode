e96a28877c11d1bebd78dd61cbabf18400cc4293
lichen <lichen@daojia-inc.com>
2018-11-27 17:38:31 +0800

    add LowestCommonAncestor

--- src/main/java/ink/lichen/leecode/bytecode/LowestCommonAncestor.java
+++ src/main/java/ink/lichen/leecode/bytecode/LowestCommonAncestor.java
@@ -1,5 +1,6 @@
 package ink.lichen.leecode.bytecode;
 
+import ink.lichen.leecode.support.ListNode;
 import ink.lichen.leecode.support.TreeNode;
 
 /**
@@ -24,12 +25,32 @@ public class LowestCommonAncestor {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || p == root || q == root)
             return root;
+
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         if (left != null && left != p && left != q)
             return left;
+
         TreeNode right = lowestCommonAncestor(root.right, p , q);
         if (left != null && right != null)
             return root;
         return left != null ? left : right;
+    }
+
+    public static void main(String[] args) {
+        TreeNode left5 = new TreeNode(5);
+        TreeNode right1 = new TreeNode(1);
+        TreeNode root = new TreeNode(3);
+        root.left = left5;
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        LowestCommonAncestor lowestCommonAncestor = new LowestCommonAncestor();
+        lowestCommonAncestor.lowestCommonAncestor(root,left5,right1);
     }
 }
