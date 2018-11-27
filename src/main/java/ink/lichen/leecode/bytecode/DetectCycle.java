2564b5d095afc1f0ebec044b4506c067058f4d0e
lichen <togetherlichen@gmail.com>
2018-11-27 08:27:33 +0800

    add detectCycle

---
+++ src/main/java/ink/lichen/leecode/bytecode/DetectCycle.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode.bytecode;
+
+import ink.lichen.leecode.support.ListNode;
+
+public class DetectCycle {
+
+
+
+    public ListNode detectCycle(ListNode head) {
+
+        return null;
+    }
+}
--- src/main/java/ink/lichen/leecode/bytecode/SortList.java
+++ src/main/java/ink/lichen/leecode/bytecode/SortList.java
@@ -9,9 +9,35 @@ public class SortList {
         if (head == null || head.next == null){
             return head;
         }
-        ListNode fast = head, slow = head;
+        ListNode fast = head, slow = head,pre = head;
+        while (fast != null && fast.next != null){
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
 
-        return null;
+        return merge(sortList(head),sortList(slow));
+    }
 
+    public ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+            }else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null){
+            cur.next = l1;
+        }
+        if (l2 != null){
+            cur.next = l2;
+        }
+        return dummy.next;
     }
 }
