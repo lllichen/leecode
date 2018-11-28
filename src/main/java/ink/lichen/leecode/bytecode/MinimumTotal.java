61c349fc1463d44e6cfeea67873f2a17919c62e5
lichen <lichen@daojia-inc.com>
2018-11-28 14:47:56 +0800

    add minimumTotal

---
+++ src/main/java/ink/lichen/leecode/bytecode/MinimumTotal.java
@@ -0,0 +1,27 @@
+package ink.lichen.leecode.bytecode;
+
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-28.
+ *
+ *
+ * 给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+ *
+ * 例如，给定三角形：
+ *
+ * [
+ *      [2],
+ *     [3,4],
+ *    [6,5,7],
+ *   [4,1,8,3]
+ * ]
+ */
+public class MinimumTotal {
+
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+
+        return 0;
+    }
+}
