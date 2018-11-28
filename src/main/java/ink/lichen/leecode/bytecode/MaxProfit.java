df18a44dd187cce54918f7b6020811629095080b
lichen <lichen@daojia-inc.com>
2018-11-28 11:18:28 +0800

    add maxProfit

--- src/main/java/ink/lichen/leecode/bytecode/MaxProfit.java
+++ src/main/java/ink/lichen/leecode/bytecode/MaxProfit.java
@@ -12,12 +12,24 @@ package ink.lichen.leecode.bytecode;
  * 输出: 5
  * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
  *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ *      dp【i】=Max（dp【i-1】，nums【i】-min）
  */
 public class MaxProfit {
 
     public int maxProfit(int[] prices) {
+        int n  =prices.length;
+        if (n <= 1){
+            return 0;
+        }
+        int max = 0;
+        int min = prices[0];
+        for (int val : prices){
+            if (min > val){
+                min = val;
+            }
+            max = (val - min) > max ? (val - min) : max;
+        }
 
-
-        return 0;
+        return max;
     }
 }
