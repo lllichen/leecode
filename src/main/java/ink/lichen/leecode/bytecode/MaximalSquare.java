13139f3d40d7e8c0fa2cb8cc25b9a183867fd9d5
lichen <lichen@daojia-inc.com>
2018-11-28 11:31:14 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/bytecode/MaximalSquare.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.bytecode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-28.
+ */
+public class MaximalSquare {
+
+
+    public int maximalSquare(char[][] matrix) {
+
+    }
+}
