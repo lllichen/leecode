8ab06eb50fc5ca72abe9944ef97dc329de77d11b
lichen <lichen@daojia-inc.com>
2018-11-28 17:31:49 +0800

    add info

--- src/main/java/ink/lichen/leecode/bytecode/MaxEnvelopes.java
+++ src/main/java/ink/lichen/leecode/bytecode/MaxEnvelopes.java
@@ -25,7 +25,12 @@ public class MaxEnvelopes {
         }
         int max = 1;
 
+        int[] buckets= new int[n];
+
         for (int i = 0 ; i < n; i++) {
+            for (int j = 0 ; j!=i && j < n; j++){
+
+            }
         }
 
         return 0;
