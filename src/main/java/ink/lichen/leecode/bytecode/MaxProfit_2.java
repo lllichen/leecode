3557a1dea1c42b51771ca2c0f8acc50120d10574
lichen <lichen@daojia-inc.com>
2018-11-28 11:30:06 +0800

    add info

--- src/main/java/ink/lichen/leecode/bytecode/MaxProfit_2.java
+++ src/main/java/ink/lichen/leecode/bytecode/MaxProfit_2.java
@@ -11,12 +11,27 @@ package ink.lichen.leecode.bytecode;
  * 输出: 7
  * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
  *      随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ *  f(n) = f(n-1)+max((price[n] - price[n-1]),0) (n > 0);
+ *  f(0) = 0;
+ *  f(1) = 0 +0 = 0
+ *  f(2) = 0 + 4 = 4
  *
  */
 public class MaxProfit_2 {
 
     public int maxProfit(int[] prices) {
 
-        return 0;
+        int n = prices.length;
+        if (n <=1){
+            return 0;
+        }
+        int profit = 0;
+        for (int i = 1 ; i <prices.length; i++){
+            int val = prices[i] - prices[i-1];
+            if (val > 0){
+                profit += val;
+            }
+        }
+        return profit;
     }
 }
