c13d2370540503c7daa967ee92148454489dea89
lichen <lichen@daojia-inc.com>
2018-11-29 13:39:08 +0800

    add LRUCache

---
+++ src/main/java/ink/lichen/leecode/bytecode/LRUCache.java
@@ -0,0 +1,22 @@
+package ink.lichen.leecode.bytecode;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-29.
+ * 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
+ * 写入数据 put(key, value) - 如果密钥不存在，则写入其数据值。当缓存容量达到上限时，
+ * 它应该在写入新数据之前删除最近最少使用的数据值，从而为新的数据值留出空间。
+ *
+ */
+public class LRUCache {
+
+    public LRUCache(int capacity) {
+
+    }
+    public int get(int key) {
+
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
