6fcfd41056108ada6f220b01f68f67252d05b909
lichen <lichen@daojia-inc.com>
2018-12-06 14:54:17 +0800

    add code

--- src/main/java/ink/lichen/leecode/tree/bt/PreorderTraversal.java
+++ src/main/java/ink/lichen/leecode/tree/bt/PreorderTraversal.java
@@ -4,6 +4,7 @@ import ink.lichen.leecode.support.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by lichen@daojia.com on 2018-12-6.
@@ -11,19 +12,33 @@ import java.util.List;
 public class PreorderTraversal {
 
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
+//        List<Integer> list = new ArrayList<>();
+//
+//        if (root == null){
+//            return list;
+//        }
+//        list.add(root.val);
+//        if (root.left != null){
+//            list.addAll(preorderTraversal(root.left));
+//        }
+//        if (root.right != null){
+//            list.addAll(preorderTraversal(root.right));
+//        }
+//        return list;
 
-        if (root == null){
-            return list;
-        }
-        list.add(root.val);
-        if (root.left != null){
-            list.addAll(preorderTraversal(root.left));
-        }
-        if (root.right != null){
-            list.addAll(preorderTraversal(root.right));
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = null;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            p = stack.pop();
+            list.add(p.val);
+            if (p.right != null) stack.push(p.right);
+            if (p.left != null) stack.push(p.left);
+
         }
         return list;
-
     }
+
 }
