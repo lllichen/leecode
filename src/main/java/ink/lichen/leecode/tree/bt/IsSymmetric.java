981d0be40b7131ab763b5964183eaa1807256d45
lichen <lichen@daojia-inc.com>
2018-12-06 18:19:25 +0800

    add IsSymmetric

--- src/main/java/ink/lichen/leecode/tree/bt/IsSymmetric.java
+++ src/main/java/ink/lichen/leecode/tree/bt/IsSymmetric.java
@@ -16,17 +16,22 @@ public class IsSymmetric {
 
 
     public boolean isSymmetric(TreeNode root) {
-
-        return false;
+        if (root == null) {
+            return true;
+        }
+        return help(root.left,root.right);
     }
 
     public boolean help(TreeNode left,TreeNode right){
         if (left == null && right == null){
             return true;
         }
-        if (left != null && right != null && left.val == right.val){
-            return true;
+        if (left == null || right == null){
+            return false;
         }
-        return false;
+        if (left.val != right.val){
+            return false;
+        }
+        return help(left.left,right.right)&&help(left.right,right.left);
     }
 }
