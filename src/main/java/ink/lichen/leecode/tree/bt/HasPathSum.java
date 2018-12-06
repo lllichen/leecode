60e2de55596926da9c5fd7ec87b4aeb7dee3d139
lichen <lichen@daojia-inc.com>
2018-12-06 18:22:14 +0800

    add HasPathSum

---
+++ src/main/java/ink/lichen/leecode/tree/bt/HasPathSum.java
@@ -0,0 +1,23 @@
+package ink.lichen.leecode.tree.bt;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-6.
+ *
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ *
+ */
+public class HasPathSum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+
+        return false;
+    }
+}
