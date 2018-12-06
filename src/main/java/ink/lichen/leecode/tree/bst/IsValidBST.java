52b5e2246b9b23052b22522fe853fc1a71a6a22a
lichen <lichen@daojia-inc.com>
2018-12-06 14:20:17 +0800

    add config

--- src/main/java/ink/lichen/leecode/tree/bst/IsValidBST.java
+++ src/main/java/ink/lichen/leecode/tree/bst/IsValidBST.java
@@ -10,48 +10,7 @@ public class IsValidBST {
 
     public boolean isValidBST(TreeNode root) {
         if (root == null){
-            return false;
-        }
-        int val = root.val;
-        TreeNode left = root.left;
-        TreeNode right = root.right;
-        if (left != null){
-            boolean flag = isValidBST(root.left);
-            if (!flag){
-                return flag;
-            }
-        }
-        if (right != null){
-            boolean flag = isValidBST(root.right);
-            if (!flag){
-                return flag;
-            }
-        }
-
-        if (left != null && right != null){
-            if (left.val <= val && val  <= right.val ){
-                return true;
-            }else {
-                return false;
-            }
-        }
-        if (left == null && right == null){
             return true;
-        }
-        if (left == null){
-            if (val <= right.val){
-                return true;
-            }else {
-                return false;
-            }
-        }
-
-        if (right == null){
-            if (left.val <= val){
-                return true;
-            }else {
-                return false;
-            }
         }
 
         return true;
