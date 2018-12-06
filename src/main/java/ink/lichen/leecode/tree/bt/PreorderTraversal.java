6932f31c28dd2733ef2ba5a5e4cf58352e44e7f0
lichen <lichen@daojia-inc.com>
2018-12-06 14:58:01 +0800

    fix error

--- src/main/java/ink/lichen/leecode/tree/bt/PreorderTraversal.java
+++ src/main/java/ink/lichen/leecode/tree/bt/PreorderTraversal.java
@@ -11,34 +11,38 @@ import java.util.Stack;
  */
 public class PreorderTraversal {
 
+
+    public void help(TreeNode node, List<Integer> list){
+        if (node != null)
+            list.add(node.val);
+        help(node.left,list);
+        help(node.right,list);
+    }
+
     public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+
+        if (root == null){
+            return list;
+        }
+        help(root,list);
+        return list;
+
 //        List<Integer> list = new ArrayList<>();
+//        if (root == null) return list;
+//        Stack<TreeNode> stack = new Stack<>();
+//        TreeNode p = null;
+//        stack.push(root);
+//        while (!stack.isEmpty()) {
+//            p = stack.pop();
+//            list.add(p.val);
+//            if (p.right != null) stack.push(p.right);
+//            if (p.left != null) stack.push(p.left);
 //
-//        if (root == null){
-//            return list;
-//        }
-//        list.add(root.val);
-//        if (root.left != null){
-//            list.addAll(preorderTraversal(root.left));
-//        }
-//        if (root.right != null){
-//            list.addAll(preorderTraversal(root.right));
 //        }
 //        return list;
-
-        List<Integer> list = new ArrayList<>();
-        if (root == null) return list;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode p = null;
-        stack.push(root);
-        while (!stack.isEmpty()) {
-            p = stack.pop();
-            list.add(p.val);
-            if (p.right != null) stack.push(p.right);
-            if (p.left != null) stack.push(p.left);
-
-        }
-        return list;
     }
+
+
 
 }
