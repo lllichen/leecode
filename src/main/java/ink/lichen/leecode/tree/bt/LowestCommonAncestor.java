dec6cf4412900eef414d644b516f8fe4c2ac0849
lichen <togetherlichen@gmail.com>
2018-12-08 08:39:45 +0800

    add LowestCommonAncestor

---
+++ src/main/java/ink/lichen/leecode/tree/bt/LowestCommonAncestor.java
@@ -0,0 +1,11 @@
+package ink.lichen.leecode.tree.bt;
+
+import ink.lichen.leecode.support.TreeNode;
+
+public class LowestCommonAncestor {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+}
