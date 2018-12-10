3a64c69367259994384929352171d431b7271a9a
lichen <lichen@daojia-inc.com>
2018-12-10 09:12:20 +0800

    add searchBST

---
+++ src/main/java/ink/lichen/leecode/tree/bst/SearchBST.java
@@ -0,0 +1,17 @@
+package ink.lichen.leecode.tree.bst;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-10.
+ */
+public class SearchBST {
+
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        TreeNode node = null;
+
+        return node;
+    }
+}
