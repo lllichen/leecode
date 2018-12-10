df32e7e6fede16a17e2416eb610dd4b1114df4d8
lichen <lichen@daojia-inc.com>
2018-12-10 09:14:01 +0800

    add tree

--- src/main/java/ink/lichen/leecode/tree/bst/SearchBST.java
+++ src/main/java/ink/lichen/leecode/tree/bst/SearchBST.java
@@ -4,6 +4,20 @@ import ink.lichen.leecode.support.TreeNode;
 
 /**
  * Created by lichen@daojia.com on 2018-12-10.
+ * 给定二叉搜索树:
+ *
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ *
+ * 和值: 2
+ *
+ *       2
+ *      / \
+ *     1   3
+ *
  */
 public class SearchBST {
 
