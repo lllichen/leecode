960f525d6ac69146c72902bfccd6915d0f53f94e
lichen <lichen@daojia-inc.com>
2018-12-10 10:49:52 +0800

    add containsNearByAlmostDuplicat

---
+++ src/main/java/ink/lichen/leecode/tree/bst/ContainsNearbyAlmostDuplicate.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.tree.bst;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-10.
+ */
+public class ContainsNearbyAlmostDuplicate {
+
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+
+        return false;
+    }
+}
--- src/main/java/ink/lichen/leecode/tree/bst/LowestCommonAncestor.java
+++ src/main/java/ink/lichen/leecode/tree/bst/LowestCommonAncestor.java
@@ -9,7 +9,15 @@ public class LowestCommonAncestor {
 
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        return null;
+        if (root == null){
+            return null;
+        }
+        if (p.val > root.val && q.val > root.val ){
+            return lowestCommonAncestor(root.right,p,q);
+        }else if (p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }else {
+            return root;
+        }
     }
 }
