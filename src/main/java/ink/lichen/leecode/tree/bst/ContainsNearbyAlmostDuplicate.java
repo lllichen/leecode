da9fd4822811af158cf9ce2c983b510c1497b9d7
lichen <lichen@daojia-inc.com>
2018-12-10 11:06:00 +0800

    add isBalanced

--- src/main/java/ink/lichen/leecode/tree/bst/ContainsNearbyAlmostDuplicate.java
+++ src/main/java/ink/lichen/leecode/tree/bst/ContainsNearbyAlmostDuplicate.java
@@ -18,21 +18,16 @@ import java.util.Map;
 public class ContainsNearbyAlmostDuplicate {
 
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-            if (k < 1 || t < 0) return false;
-            Map<Long, Long> map = new HashMap<>();
-            for (int i = 0; i < nums.length; i++) {
-                long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
-                long bucket = remappedNum / ((long) t + 1);
-                if (map.containsKey(bucket)
-                        || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t)
-                        || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t))
-                    return true;
-                if (map.entrySet().size() >= k) {
-                    long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
-                    map.remove(lastBucket);
-                }
-                map.put(bucket, remappedNum);
-            }
+        if (k < 0 || nums.length > 1000) {
             return false;
+        }
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length && j <= i + k; j++) {
+                if (Math.abs((long)nums[i] - nums[j]) <= t) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
---
+++ src/main/java/ink/lichen/leecode/tree/bst/IsBalanced.java
@@ -0,0 +1,15 @@
+package ink.lichen.leecode.tree.bst;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-10.
+ */
+public class IsBalanced {
+
+    public boolean isBalanced(TreeNode root) {
+
+        return false;
+    }
+
+}
