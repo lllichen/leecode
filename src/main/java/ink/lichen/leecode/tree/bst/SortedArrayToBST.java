e8e996e86462f247df485c7b8083a5ee4cb3ab60
lichen <lichen@daojia-inc.com>
2018-12-10 11:25:05 +0800

    add SortedArrayToBST

--- src/main/java/ink/lichen/leecode/tree/bst/SortedArrayToBST.java
+++ src/main/java/ink/lichen/leecode/tree/bst/SortedArrayToBST.java
@@ -4,11 +4,36 @@ import ink.lichen.leecode.support.TreeNode;
 
 /**
  * Created by lichen@daojia.com on 2018-12-10.
+ *
+ * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ *
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * 给定有序数组: [-10,-3,0,5,9],
+ *
+ * 一个可能的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：
+ *
+ *       0
+ *      / \
+ *    -3   9
+ *    /   /
+ *  -10  5
+ *
  */
 public class SortedArrayToBST {
 
     public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBST(nums,0,nums.length-1);
+    }
 
-        return null;
+    private TreeNode sortedArrayToBST(int[] nums, int left, int right){
+        if (left > right) {
+            return null;
+        }
+        int mid = (left +right)>>1;
+        TreeNode cur = new TreeNode(nums[mid]);
+        cur.left = sortedArrayToBST(nums,left,mid);
+        cur.right = sortedArrayToBST(nums,mid,right);
+        return cur;
     }
 }
