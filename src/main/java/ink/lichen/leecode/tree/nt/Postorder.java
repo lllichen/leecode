0a4272a474ec5a89725dccc63cd33c5fc0d41122
lichen <lichen@daojia-inc.com>
2018-12-15 15:14:25 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/tree/nt/Postorder.java
@@ -0,0 +1,23 @@
+package ink.lichen.leecode.tree.nt;
+
+import ink.lichen.leecode.support.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-15.
+ *               1
+ *            3  2  4
+ *          5  6
+ *
+ *        返回其后序遍历:[5,6,3,2,4,1]
+ */
+public class Postorder {
+
+
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        return res;
+    }
+}
--- src/main/java/ink/lichen/leecode/tree/nt/Preorder.java
+++ src/main/java/ink/lichen/leecode/tree/nt/Preorder.java
@@ -4,10 +4,15 @@ import ink.lichen.leecode.support.Node;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by lichen@daojia.com on 2018-12-15.
+ *             1
+ *          3  2  4
+ *        5  6
  *
+ *      返回其前序遍历:[1,3,5,6,2,4]
  */
 public class Preorder {
 
@@ -30,6 +35,22 @@ public class Preorder {
 
     public List<Integer> preorder(Node root) {
         List<Integer> res = new ArrayList<>();
+        if (root == null){
+            return res;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()){
+            Node node = stack.pop();
+            res.add(node.val);
+            List<Node> children = node.children;
+
+            for (int i = children.size()-1;i >=0; i--){
+                if (children.get(i) != null){
+                    stack.push(children.get(i));
+                }
+            }
+        }
         return res;
     }
 }
