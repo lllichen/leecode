da2cecbd5177fadfefd3fc30cf56d442bd0f01c8
lichen <lichen@daojia-inc.com>
2018-12-17 15:52:02 +0800

    fix error

--- src/main/java/ink/lichen/leecode/bytecode/LengthOfLongestSubstring.java
+++ src/main/java/ink/lichen/leecode/bytecode/LengthOfLongestSubstring.java
@@ -1,16 +1,19 @@
 package ink.lichen.leecode.bytecode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Created by lichen@daojia.com on 2018-11-23.
  * 输入: "abcabcbb"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
  *
- *
- *
+ * i = 0; j = 0; ans = 1; index['a'] = 1;
+ * i = 0; j = 1; ans = 2; index['b'] = 2;
+ * i = 0; j = 2; ans = 3; index['c'] = 3;
+ * i = 1; j = 3; ans = 3; index['a'] = 4;
+ * i = 2; j = 4; ans = 3; index['b'] = 5;
+ * i = 3; j = 5; ans = 3; index['c'] = 6;
+ * i = 5; j = 6; ans = 3; index['b'] = 7;
+ * i = 6; j = 7; ans = 3; index['b'] = 8;
  */
 public class LengthOfLongestSubstring {
 
--- src/main/java/ink/lichen/leecode/bytecode/LongestCommonPrefix.java
+++ src/main/java/ink/lichen/leecode/bytecode/LongestCommonPrefix.java
@@ -9,42 +9,14 @@ package ink.lichen.leecode.bytecode;
 public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
-
-        int n = strs.length;
-        if (n==0){
-            return "";
-        }
-        if (n == 1){
-            return strs[0];
-        }
-
-        int min = Integer.MAX_VALUE;
-        for (String temp : strs){
-            if (temp.length()<min){
-                min = temp.length();
-            }
-            if (min == 0){
-                return "";
-            }
-            if (min == 1){
-                return temp;
-            }
-        }
-
-        int i = 0;
-
-        while (i<min){
-            char val = strs[0].charAt(i);
-            for (int j = 1 ; j < n; j++ ){
-                if(strs[j].charAt(i) != val){
-                    return strs[0].substring(0,i);
-                }
-            }
+        if(strs == null || strs.length == 0)    return "";
+        String pre = strs[0];
+        int i = 1;
+        while(i < strs.length){
+            while(strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0,pre.length()-1);
             i++;
         }
-        if (i == min){
-            return strs[0].substring(0,i);
-        }
-        return "";
+        return pre;
     }
 }
