d70c1d08a2c7e8048759153e14b57f9ad01d42ae
lichen <lichen@daojia-inc.com>
2018-12-25 14:05:17 +0800

    add GetPermutation

---
+++ src/main/java/ink/lichen/leecode/test/_12_25/GetPermutation.java
@@ -0,0 +1,36 @@
+package ink.lichen.leecode.test._12_25;
+
+/**
+ * Created by lichen@daojia.com on 2018-11-26.
+ *
+ *
+     给出集合 [1,2,3,…,n]，其所有元素共有 n! 种排列。
+
+     按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+
+ *   "123"
+ *   "132"
+ *
+ *   "213"
+ *   "231"
+ *
+ *   "312"
+ *   "321"
+ *
+ * 输入: n = 3, k = 3
+ * 输出: "213"
+ *
+ */
+public class GetPermutation {
+
+    public String getPermutation(int n, int k) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        GetPermutation getPermutation = new GetPermutation();
+        System.out.println(getPermutation.getPermutation(3,3));
+    }
+
+}
