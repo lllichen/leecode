a03da3ccf51b8f4b4a3476f7d07b7f4350031e98
lichen <togetherlichen@gmail.com>
2018-12-26 06:22:59 +0800

    add FindCircleNum

--- src/main/java/ink/lichen/leecode/test/_12_25/FindCircleNum.java
+++ src/main/java/ink/lichen/leecode/test/_12_25/FindCircleNum.java
@@ -6,8 +6,24 @@ package ink.lichen.leecode.test._12_25;
 public class FindCircleNum {
 
     public int findCircleNum(int[][] M) {
+        int num = 0;
+        int n = M.length;
+        boolean[] hasVisited = new boolean[n];
+        for (int i = 0 ; i < n ; i++){
+            if (!hasVisited[i]){
+                dfs(M,i,hasVisited);
+                num++;
+            }
+        }
+        return num;
+    }
 
-
-        return 0;
+    public void dfs(int[][] m, int i, boolean[] hasVisited){
+        hasVisited[i] = true;
+        for (int k = 0 ; k < m.length; k++){
+            if (m[i][k] == 1 && !hasVisited[k]){
+                dfs(m,k,hasVisited);
+            }
+        }
     }
 }
