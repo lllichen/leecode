3ce73ad1cc6fc940d738568fb893cacfe577cccd
lichen <togetherlichen@gmail.com>
2018-12-26 22:49:01 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_12_26/DetectCycle.java
@@ -0,0 +1,11 @@
+package ink.lichen.leecode.test.test_12_26;
+
+import ink.lichen.leecode.support.ListNode;
+
+public class DetectCycle {
+
+
+    public ListNode detectCycle(ListNode head) {
+
+    }
+}
