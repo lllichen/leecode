c1eef4eb7c0c62c0fd1119adc78a61ac98402900
lichen <lichen@daojia-inc.com>
2018-12-29 16:07:46 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_12_29/ZigzagLevelOrder.java
@@ -0,0 +1,17 @@
+package ink.lichen.leecode.test.test_12_29;
+
+import ink.lichen.leecode.support.TreeNode;
+
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-29.
+ */
+public class ZigzagLevelOrder {
+
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        return null;
+    }
+}
