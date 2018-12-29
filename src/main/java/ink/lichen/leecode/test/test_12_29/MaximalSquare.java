27cca931fa2e540c87c5ed7b456e8e56717e126e
lichen <lichen@daojia-inc.com>
2018-12-29 18:21:37 +0800

    add MaximalSquatre

--- src/main/java/ink/lichen/leecode/test/test_12_29/MaximalSquare.java
+++ src/main/java/ink/lichen/leecode/test/test_12_29/MaximalSquare.java
@@ -2,6 +2,16 @@ package ink.lichen.leecode.test.test_12_29;
 
 /**
  * Created by lichen@daojia.com on 2018-12-29.
+ *
+ * 在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+ *
+ * 1 0 1 0 0
+ * 1 0 1 1 1
+ * 1 1 1 1 1
+ * 1 0 0 1 0
+ *
+ * 输出4
+ *
  */
 public class MaximalSquare {
 
