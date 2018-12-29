e5290cf8859f24e39b283b78ba7e42eeff8aed26
lichen <lichen@daojia-inc.com>
2018-12-29 11:56:20 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_12_29/MergeKLists.java
+++ src/main/java/ink/lichen/leecode/test/test_12_29/MergeKLists.java
@@ -8,7 +8,17 @@ import ink.lichen.leecode.support.ListNode;
 public class MergeKLists {
 
     public ListNode mergeKLists(ListNode[] lists) {
-
+        int left = 0, right = lists.length-1;
+        mergeKLists(lists,left,right);
         return null;
+    }
+
+    private void mergeKLists(ListNode[] lists, int left, int right) {
+        if (left < right){
+            int center = (left + right)>>1;
+            mergeKLists(lists,left,center);
+            mergeKLists(lists,center+1,right);
+//            merge(lists,)
+        }
     }
 }
