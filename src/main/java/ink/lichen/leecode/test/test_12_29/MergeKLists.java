c73c41ce75278558a5f9b7ef01cdf5e058b19a3d
lichen <lichen@daojia-inc.com>
2018-12-29 11:52:04 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/test/test_12_29/MergeKLists.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.test.test_12_29;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-29.
+ */
+public class MergeKLists {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        return null;
+    }
+}
