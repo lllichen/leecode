dc8eb9a3dfb2c2fb54b883ec7c427cabce634872
lichen <lichen@daojia-inc.com>
2018-12-29 15:46:06 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_12_29/LowestCommonAncestor.java
@@ -0,0 +1,18 @@
+package ink.lichen.leecode.test.test_12_29;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2018-12-29.
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先
+ *
+ */
+public class LowestCommonAncestor {
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && )
+
+        return null;
+    }
+}
