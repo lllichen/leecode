5c227ac8af65a5d853fac5ea37ffc7d15e944b76
lichen <lichen@daojia-inc.com>
2019-01-02 12:30:36 +0800

    add code

--- src/main/java/ink/lichen/leecode/test/test_1_1/AllOne.java
+++ src/main/java/ink/lichen/leecode/test/test_1_1/AllOne.java
@@ -1,22 +1,68 @@
 package ink.lichen.leecode.test.test_1_1;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class AllOne {
 
+//    1,2,3,4,5,6,7,8,9,10
+
+    private static class Node{
+
+        Set<String> set = new HashSet<>();
+
+        public Node(Node pre, Node next) {
+            this.pre = pre;
+            this.next = next;
+        }
+
+        Node pre;
+
+        Node next;
+
+        void add(String val){
+            set.add(val);
+        }
+
+        void remove(String val){
+            set.remove(val);
+        }
+
+        String val(){
+            return set.iterator().next();
+        }
+    }
+
+    Node max;
+    Node min;
+
+
+    Map<String,Integer> map;
 
 
     /**
      * Initialize your data structure here.
      */
     public AllOne() {
-
+        map = new HashMap<>();
+        max = new Node(null,null);
+        min = new Node(max,null);
+        max.next = min;
     }
 
     /**
      * Inserts a new key <Key> with value 1. Or increments an existing key by 1.
      */
     public void inc(String key) {
-
+        Integer val = map.get(key) ;
+        if (val != null){
+            Integer old = val;
+            map.put(key,val+1);
+        }
     }
+
 
     /**
      * Decrements an existing key by 1. If Key's value is 1, remove it from the data structure.
@@ -29,8 +75,12 @@ public class AllOne {
      * Returns one of the keys with maximal value.
      */
     public String getMaxKey() {
-
-        return "";
+        Node node = max.next;
+        if (node == min){
+            return "";
+        }else {
+            return node.val();
+        }
     }
 
     /**
@@ -38,6 +88,11 @@ public class AllOne {
      */
     public String getMinKey() {
 
-        return "";
+        Node node = min.pre;
+        if (node == max){
+            return "";
+        }else {
+            return node.val();
+        }
     }
 }
