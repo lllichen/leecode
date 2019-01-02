45671524f9238ca423ec9c1177b9ff10c29971e1
lichen <lichen@daojia-inc.com>
2019-01-02 18:51:05 +0800

    fix AllOne

--- src/main/java/ink/lichen/leecode/test/test_1_1/AllOne.java
+++ src/main/java/ink/lichen/leecode/test/test_1_1/AllOne.java
@@ -9,7 +9,7 @@ public class AllOne {
 
 //    1,2,3,4,5,6,7,8,9,10
 
-    private static class Node{
+    private static class Node {
 
         Set<String> set = new HashSet<>();
 
@@ -22,19 +22,19 @@ public class AllOne {
 
         Node next;
 
-        void add(String val){
+        void add(String val) {
             set.add(val);
         }
 
-        void remove(String val){
+        void remove(String val) {
             set.remove(val);
         }
 
-        String val(){
+        String val() {
             return set.iterator().next();
         }
 
-        boolean isEmpty(){
+        boolean isEmpty() {
             return set.isEmpty();
         }
     }
@@ -43,9 +43,9 @@ public class AllOne {
     Node min;
 
 
-    Map<String,Integer> map;
+    Map<String, Integer> map;
 
-    Map<Integer,Node> nodeMap;
+    Map<Integer, Node> nodeMap;
 
 
     /**
@@ -53,9 +53,9 @@ public class AllOne {
      */
     public AllOne() {
         map = new HashMap<>();
-        nodeMap = new HashMap<Integer,Node>();
-        min = new Node(null,null);
-        max = new Node(min,null);
+        nodeMap = new HashMap<Integer, Node>();
+        min = new Node(null, null);
+        max = new Node(min, null);
         min.next = max;
     }
 
@@ -63,38 +63,38 @@ public class AllOne {
      * Inserts a new key <Key> with value 1. Or increments an existing key by 1.
      */
     public void inc(String key) {
-        Integer val = map.get(key) ;
-        if (val != null){
+        Integer val = map.get(key);
+        if (val != null) {
             Integer oldVal = val;
             Node oldNode = nodeMap.get(oldVal);
             oldNode.remove(key);
-            if (oldNode.isEmpty()){
+            if (oldNode.isEmpty()) {
                 oldNode.pre.next = oldNode.next;
                 oldNode.next.pre = oldNode.pre;
                 oldNode = oldNode.pre;
+                //delete null old
+                nodeMap.remove(oldVal);
             }
-
-            Integer newVal = val+1;
-            map.put(key,val+1);
+            Integer newVal = val + 1;
+            map.put(key, newVal);
             Node newNode = nodeMap.get(newVal);
             if (newNode == null) {
                 newNode = new Node(oldNode, oldNode.next);
                 //mark
                 oldNode.next.pre = oldNode.next = newNode;
-
                 nodeMap.put(newVal, newNode);
             }
-                newNode.add(key);
+            newNode.add(key);
 
-        }else {
-            map.put(key,1);
+        } else {
+            map.put(key, 1);
             Node node = nodeMap.get(1);
-            if (node == null){
-                node = new Node(min,min.next);
+            if (node == null) {
+                node = new Node(min, min.next);
                 min.next.pre = min.next = node;
                 node.add(key);
-                nodeMap.put(1,node);
-            }else {
+                nodeMap.put(1, node);
+            } else {
                 node.add(key);
             }
         }
@@ -106,33 +106,35 @@ public class AllOne {
      */
     public void dec(String key) {
         Integer val = map.get(key);
-        if (val == null){
+        if (val == null) {
             return;
         }
-        if (val == 1){
+        if (val == 1) {
             map.remove(key);
             Node old = min.next;
             old.remove(key);
-            if (old.isEmpty()){
+            if (old.isEmpty()) {
                 nodeMap.remove(1);
                 min.next = old.next;
                 old.next.pre = min;
             }
-        }else {
+        } else {
             Node old = nodeMap.get(val);
             old.remove(key);
-            if (old.isEmpty()){
+            if (old.isEmpty()) {
                 old.pre.next = old.next;
                 old.next.pre = old.pre;
                 old = old.next;
+                nodeMap.remove(val);
             }
-            val = val-1;
-            map.put(key,val);
+            val = val - 1;
+            map.put(key, val);
             Node newNode = nodeMap.get(val);
             //check isEmpty
-            if (newNode.isEmpty()){
-                newNode = new Node(old.pre,old);
+            if (newNode == null) {
+                newNode = new Node(old.pre, old);
                 old.pre.next = old.pre = newNode;
+                nodeMap.put(val, newNode);
             }
             newNode.add(key);
         }
@@ -143,9 +145,9 @@ public class AllOne {
      */
     public String getMaxKey() {
         Node node = max.pre;
-        if (node == min){
+        if (node == min) {
             return "";
-        }else {
+        } else {
             return node.val();
         }
     }
@@ -156,9 +158,9 @@ public class AllOne {
     public String getMinKey() {
 
         Node node = min.next;
-        if (node == max){
+        if (node == max) {
             return "";
-        }else {
+        } else {
             return node.val();
         }
     }
@@ -167,18 +169,34 @@ public class AllOne {
 //    ["AllOne","inc","inc","inc","dec","inc","inc","getMaxKey","dec","dec","dec","getMaxKey"]
 //            [[],["hello"],["world"],["hello"],["world"],["hello"],["leet"],[],["hello"],["hello"],["hello"],[]]
 
+//    ["AllOne","inc","inc","inc","inc","getMaxKey","inc","inc","inc","dec","inc","inc","inc","getMaxKey"]
+//            [[],["hello"],["goodbye"],["hello"],["hello"],[],["leet"],["code"],["leet"],["hello"],["leet"],["code"],["code"],[]]
+
     public static void main(String[] args) {
         AllOne allOne = new AllOne();
-        allOne.inc("hello");
-        allOne.inc("world");
-        allOne.inc("hello");
-        allOne.dec("world");
-        allOne.inc("hello");
-        allOne.inc("leet");
+//        allOne.inc("hello"); //hello 1
+//        allOne.inc("world"); //world 1
+//        allOne.inc("hello"); //hello 2
+//        allOne.dec("world"); //world 0
+//        allOne.inc("hello"); //hello 3
+//        allOne.inc("leet"); //leet 1
+//        System.out.println(allOne.getMaxKey());
+//        allOne.dec("hello"); //hello 2
+//        allOne.dec("hello"); //hello 1
+//        allOne.dec("hello"); //hello 0
+//        System.out.println(allOne.getMaxKey());
+        allOne.inc("hello"); //hello 1
+        allOne.inc("goodbye"); //goodbye 1
+        allOne.inc("hello"); // hello 2
+        allOne.inc("hello"); // hello 3
         System.out.println(allOne.getMaxKey());
-        allOne.dec("hello");
-        allOne.dec("hello");
-        allOne.dec("hello");
+        allOne.inc("leet"); // leet 1
+        allOne.inc("code"); // code 1
+        allOne.inc("leet"); // leet 2
+        allOne.dec("hello");// hello 2
+        allOne.inc("leet"); // leet 3
+        allOne.inc("code"); // code 2
+        allOne.inc("code"); // code 3
         System.out.println(allOne.getMaxKey());
     }
 }
