6aaaefaec86367c1c6c32c5aa3dc4808919bbfa7
lichen <lichen@daojia-inc.com>
2019-01-02 20:04:40 +0800

    add sqrt and validUtf8

---
+++ src/main/java/ink/lichen/leecode/test/test_1_2/ValidUtf8.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode.test.test_1_2;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-2.
+ */
+public class ValidUtf8 {
+
+
+    public boolean validUtf8(int[] data) {
+
+        return false;
+    }
+}
