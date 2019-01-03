69185d035cbb60d2046c6c7de954841571ef0e1a
lichen <lichen@daojia-inc.com>
2019-01-03 11:22:24 +0800

    add LongestPallindrome

---
+++ src/main/java/ink/lichen/leecode/test/test_1_3/LongestPalindrome.java
@@ -0,0 +1,17 @@
+package ink.lichen.leecode.test.test_1_3;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-3.
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ */
+public class LongestPalindrome {
+
+    public String longestPalindrome(String s) {
+
+
+        return "";
+    }
+}
