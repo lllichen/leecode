9e126b5fbd420d1b24c1103495db9beb48ed25c0
lichen <lichen@daojia-inc.com>
2019-01-03 11:44:21 +0800

    add longestOalindrome

--- src/main/java/ink/lichen/leecode/test/test_1_3/LongestPalindrome.java
+++ src/main/java/ink/lichen/leecode/test/test_1_3/LongestPalindrome.java
@@ -10,8 +10,39 @@ package ink.lichen.leecode.test.test_1_3;
 public class LongestPalindrome {
 
     public String longestPalindrome(String s) {
+//        String res = "";
+//        int n = s.length();
+//        if ( n == 0){
+//            return res;
+//        }
+//        for (int i = 0 ; i < n ; i++ ){
+//
+//        }
+//
+//        return "";
 
+        int n=s.length();
+        boolean[][] pal=new boolean[n][n];
+        //pal[i][j] 表示s[i...j]是否是回文串
+//        int maxLen=0;
+        String res = "";
+        for (int i=0;i<n;i++){  // i作为终点
+            int j=i;    //j作为起点
+            while (j>=0){
+                if (s.charAt(j)==s.charAt(i)&&(i-j<2||pal[j+1][i-1])){
+                    pal[j][i]=true;
+                    if (res.length() < i-j + 1){
+                         res = s.substring(j,i+1);
+                    }
+                }
+                j--;
+            }
+        }
+        return res;
+    }
 
-        return "";
+    public static void main(String[] args) {
+        LongestPalindrome longestPalindrome = new LongestPalindrome();
+        System.out.println(longestPalindrome.longestPalindrome("abaca1ba"));
     }
 }
