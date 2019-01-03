511fbf030ecd1a14d2437ee4912ddfd08c6a3475
lichen <lichen@daojia-inc.com>
2019-01-03 18:26:31 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_1_3/MyAtoi.java
+++ src/main/java/ink/lichen/leecode/test/test_1_3/MyAtoi.java
@@ -1,5 +1,7 @@
 package ink.lichen.leecode.test.test_1_3;
 
+import java.lang.annotation.Inherited;
+
 /**
  * Created by lichen@daojia.com on 2019-1-3.
  */
@@ -13,16 +15,54 @@ public class MyAtoi {
         //begin
         int i;
         //trim space
-        for (i =0; i <= n ;i ++){
-            if (str.charAt(i) == ' '){
-                i++;
-            }else {
+        for (i =0; i < n ;i ++){
+            if (str.charAt(i) != ' '){
                 break;
             }
         }
+        if (i== n){
+            return 0;
+        }
+        boolean negative = false;
+        int res = 0;
+        if (str.charAt(i) == '+' || str.charAt(i) == '-' || (str.charAt(i) <= '9' && str.charAt(i)>='0' )){
+            if (str.charAt(i) == '-'){
+                negative = true;
+                i++;
+            }else  if (str.charAt(i) == '+'){
+                i++;
+            }
+            for (;i < n; i++){
+                if (str.charAt(i) > '9' || str.charAt(i) < '0'){
+                    if (negative){
+                        return -res;
+                    }else
+                        return res;
+                }
+                if (res < Integer.MAX_VALUE/10){
+                        res = (str.charAt(i)-'0')+res*10;
+                }else {
+                    if (res == Integer.MAX_VALUE/10 && str.charAt(i) - '0' <= Integer.MAX_VALUE % 10 ) {
+                            res = (str.charAt(i) - '0') + res * 10;
+                    }else {
+                        if (negative){
+                            return Integer.MIN_VALUE;
+                        }else
+                            return Integer.MAX_VALUE;
+                    }
+                }
+            }
+            if (negative){
+                return -res;
+            }else
+                return res;
+        }else{
+            return 0;
+        }
+    }
 
-
-
-        return 10;
+    public static void main(String[] args) {
+        MyAtoi myAtoi = new MyAtoi();
+        System.out.println(myAtoi.myAtoi("2147483648"));
     }
 }
