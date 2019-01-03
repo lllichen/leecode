7d46cb7d71e862635ff7757251288b3819fbc30e
lichen <lichen@daojia-inc.com>
2019-01-03 17:55:03 +0800

    add myAtoi

--- src/main/java/ink/lichen/leecode/test/test_1_3/MyAtoi.java
+++ src/main/java/ink/lichen/leecode/test/test_1_3/MyAtoi.java
@@ -9,16 +9,20 @@ public class MyAtoi {
     public int myAtoi(String str) {
         if (str == null || str.length() == 0)
             return 0;
-        boolean isNegative = str.charAt(0) == '-';
-        int ret = 0;
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (i == 0 && (c == '+' || c == '-'))  /* 符号判定 */
-                continue;
-            if (c < '0' || c > '9')                /* 非法输入 */
-                return 0;
-            ret = ret * 10 + (c - '0');
+        int n = str.length();
+        //begin
+        int i;
+        //trim space
+        for (i =0; i <= n ;i ++){
+            if (str.charAt(i) == ' '){
+                i++;
+            }else {
+                break;
+            }
         }
-        return isNegative ? -ret : ret;
+
+
+
+        return 10;
     }
 }
