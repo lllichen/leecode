993d4a64456eaad22a0f7367b3a415832bda967d
lichen <lichen@daojia-inc.com>
2019-01-04 12:30:10 +0800

    add comment

--- src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSumClosest.java
+++ src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSumClosest.java
@@ -2,6 +2,15 @@ package ink.lichen.leecode.test.test_1_4;
 
 /**
  * Created by lichen@daojia.com on 2019-1-4.
+ *
+ * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+ *
+ * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
  */
 public class ThreeSumClosest {
+
+    public int threeSumClosest(int[] nums, int target) {
+
+        return 0;
+    }
 }
