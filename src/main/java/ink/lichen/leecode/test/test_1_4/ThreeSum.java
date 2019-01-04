4d4acc256e1a502cde245dcf49bc8bb618351408
lichen <lichen@daojia-inc.com>
2019-01-04 11:44:04 +0800

    add three

--- src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSum.java
+++ src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSum.java
@@ -1,5 +1,8 @@
 package ink.lichen.leecode.test.test_1_4;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,7 +12,16 @@ public class ThreeSum {
 
 
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        if (n <= 2){
+            return res;
+        }
 
+        Arrays.sort(nums);
+        for (int i = 0 ; i < nums.length; i++){
+
+        }
 
 
         return null;
