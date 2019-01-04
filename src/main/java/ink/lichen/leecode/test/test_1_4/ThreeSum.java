c439dc25ee7c56dd200733ac273f80e10610d446
lichen <lichen@daojia-inc.com>
2019-01-04 12:25:55 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSum.java
+++ src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSum.java
@@ -19,11 +19,35 @@ public class ThreeSum {
         }
 
         Arrays.sort(nums);
-        for (int i = 0 ; i < nums.length; i++){
-
+        for (int i = 0 ; i < n-2; i++){
+            help(res,nums,i);
         }
+        return res;
+    }
 
+    public void help(List<List<Integer>> res,int[] nums, int i){
+        int right = nums.length-1;
+        int left = i+1;
+        int target = nums[i];
+        while (left < right){
+            int sum = target + nums[left]+nums[right];
+            if ( sum < 0){
+                left++;
+            }else if (sum >0){
+                right--;
+            }else {
+                ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                while (left < --right && nums[right] == nums[right + 1]);
+                while (right > ++left && nums[left] == nums[left - 1]);
+                while (i < nums.length - 2 && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+            }
+        }
+    }
 
-        return null;
+    public static void main(String[] args) {
+        ThreeSum threeSum = new ThreeSum();
+        threeSum.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 }
