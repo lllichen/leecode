efc2337f0ae3cca71ffdfbcc6077582221918e7e
lichen <lichen@daojia-inc.com>
2019-01-04 12:29:14 +0800

    add TreeSumClosest

--- src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSum.java
+++ src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSum.java
@@ -20,31 +20,56 @@ public class ThreeSum {
 
         Arrays.sort(nums);
         for (int i = 0 ; i < n-2; i++){
-            help(res,nums,i);
+            int right = nums.length-1;
+            int left = i+1;
+            int target = nums[i];
+            while (left < right){
+                int sum = target + nums[left]+nums[right];
+                if ( sum < 0){
+                    left++;
+                }else if (sum >0){
+                    right--;
+                }else {
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while (left < --right && nums[right] == nums[right + 1]);
+                    while (right > ++left && nums[left] == nums[left - 1]);
+                    while (i < nums.length - 2 && nums[i] == nums[i + 1]) {
+                        i++;
+                    }
+                }
+            }
         }
         return res;
     }
 
-    public void help(List<List<Integer>> res,int[] nums, int i){
-        int right = nums.length-1;
-        int left = i+1;
-        int target = nums[i];
-        while (left < right){
-            int sum = target + nums[left]+nums[right];
-            if ( sum < 0){
-                left++;
-            }else if (sum >0){
-                right--;
-            }else {
-                ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                while (left < --right && nums[right] == nums[right + 1]);
-                while (right > ++left && nums[left] == nums[left - 1]);
-                while (i < nums.length - 2 && nums[i] == nums[i + 1]) {
-                    i++;
-                }
-            }
-        }
-    }
+
+//    public List<List<Integer>> threeSum(int[] nums) {
+
+//         Arrays.sort(nums);
+
+//         List<List<Integer>> ans = new ArrayList<>();
+//         for (int i = 0 ; i < nums.length-2;i++){
+//             int left = i+1;int right = nums.length-1;
+//             while (left< right){
+//                 int sum = nums[i] + nums[left]+nums[right];
+//                 if (sum < 0){
+//                     left++;
+//                 }else if (sum > 0){
+//                     right--;
+//                 }else{
+//                     ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+//                     while (left < --right && nums[right] == nums[right + 1]);
+//                     while (right > ++left && nums[left] == nums[left - 1]);
+//                     while (i < nums.length - 2 && nums[i] == nums[i + 1]) {
+//                         i++;
+//                     }
+//                 }
+
+//             }
+//         }
+
+//         return ans;
+//     }
 
     public static void main(String[] args) {
         ThreeSum threeSum = new ThreeSum();
---
+++ src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSumClosest.java
@@ -0,0 +1,7 @@
+package ink.lichen.leecode.test.test_1_4;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-4.
+ */
+public class ThreeSumClosest {
+}
