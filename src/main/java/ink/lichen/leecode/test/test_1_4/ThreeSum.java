6fae6008182a7dd31c84efac42b96f9c09e89471
lichen <lichen@daojia-inc.com>
2019-01-04 11:41:56 +0800

    add ThreeSum

---
+++ src/main/java/ink/lichen/leecode/test/test_1_4/ThreeSum.java
@@ -0,0 +1,17 @@
+package ink.lichen.leecode.test.test_1_4;
+
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-4.
+ */
+public class ThreeSum {
+
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+
+
+        return null;
+    }
+}
