b223656c597f7169c153fb5b953ff1713683ddbc
lichen <lichen@daojia-inc.com>
2019-01-05 18:14:53 +0800

    add produceExceptSelf

--- src/main/java/ink/lichen/leecode/test/test_1_5/ProductExceptSelf.java
+++ src/main/java/ink/lichen/leecode/test/test_1_5/ProductExceptSelf.java
@@ -4,4 +4,9 @@ package ink.lichen.leecode.test.test_1_5;
  * Created by lichen@daojia.com on 2019-1-5.
  */
 public class ProductExceptSelf {
+
+
+    public int[] productExceptSelf(int[] nums) {
+        return null;
+    }
 }
