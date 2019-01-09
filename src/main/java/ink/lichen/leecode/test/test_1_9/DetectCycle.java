4a2cbe987d5b059c665eefcfcb370404ed31f308
lichen <lichen@daojia-inc.com>
2019-01-09 17:22:32 +0800

    fix error

---
+++ src/main/java/ink/lichen/leecode/test/test_1_9/DetectCycle.java
@@ -0,0 +1,20 @@
+package ink.lichen.leecode.test.test_1_9;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-9.
+ */
+public class DetectCycle {
+
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head,slow = head;
+        while (fast != null && fast.next != null){
+
+        }
+
+
+        return null;
+    }
+}
--- src/main/java/ink/lichen/leecode/test/test_1_9/HasCycle.java
+++ src/main/java/ink/lichen/leecode/test/test_1_9/HasCycle.java
@@ -6,8 +6,15 @@ public class HasCycle {
 
 
     public boolean hasCycle(ListNode head) {
+        ListNode fast = head,slow = head;
 
-
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                return true;
+            }
+        }
         return false;
     }
 }
