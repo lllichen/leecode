ebaf76ea172f65dec004c6d726ff74222c4a0d76
lichen <lichen@daojia-inc.com>
2019-01-09 17:37:02 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_1_9/DetectCycle.java
+++ src/main/java/ink/lichen/leecode/test/test_1_9/DetectCycle.java
@@ -11,10 +11,17 @@ public class DetectCycle {
     public ListNode detectCycle(ListNode head) {
         ListNode fast = head,slow = head;
         while (fast != null && fast.next != null){
-
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                fast = head;
+                while (fast != slow){
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
         }
-
-
         return null;
     }
 }
---
+++ src/main/java/ink/lichen/leecode/test/test_1_9/GetIntersectionNode.java
@@ -0,0 +1,19 @@
+package ink.lichen.leecode.test.test_1_9;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-9.
+ */
+public class GetIntersectionNode {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while (curA != curB ){
+
+        }
+
+        return null;
+    }
+}
