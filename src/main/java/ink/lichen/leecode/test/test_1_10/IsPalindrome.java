3db63b639168df9641c4d718ddaaa46574a99ed5
lichen <togetherlichen@gmail.com>
2019-01-10 08:31:59 +0800

    add code

--- src/main/java/ink/lichen/leecode/test/test_1_10/IsPalindrome.java
+++ src/main/java/ink/lichen/leecode/test/test_1_10/IsPalindrome.java
@@ -1,41 +1,26 @@
 package ink.lichen.leecode.test.test_1_10;
 
 /**
- * 1001  100 0 100 1
+ * 1001  1001 1 100 1
  *
  */
 public class IsPalindrome {
 
 
     public boolean isPalindrome(int x) {
+        int a = x, r = 0;
 
-        if (x < 0){
-            return false;
+        if (x < 0) return false;
+
+        while (a > 0) {
+            r = r*10 + a%10;
+            a = a / 10;
         }
-        if (x < 10){
-            return true;
-        }
-        int val = 0;
-        int pre = 0;
-        while (x /10 != 0){
-            int t = x % 10;
-            pre = val;
-            val = val * 10 + t;
-            if (val == x){
-                return true;
-            }
-            x = x / 10;
-            if (pre == x || val == x){
-                return true;
-            }
-        }
-        if (val == x){
-            return true;
-        }
-        return false;
+
+        return r == x;
     }
 
     public static void main(String[] args) {
-        System.out.println(new IsPalindrome().isPalindrome(101));
+        System.out.println(new IsPalindrome().isPalindrome(1001));
     }
 }
---
+++ src/main/java/ink/lichen/leecode/test/test_1_10/MajorityElement.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode.test.test_1_10;
+
+/**
+ * 输入: [3,2,3]
+ * 输出: 3
+ */
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+
+        return 0;
+    }
+}
