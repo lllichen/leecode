17ba2234605843fce6302f5d3615f23e4b39d02a
lichen <lichen@daojia-inc.com>
2019-01-10 15:54:38 +0800

    add sortList

--- src/main/java/ink/lichen/leecode/test/test_1_10/MajorityElement.java
+++ src/main/java/ink/lichen/leecode/test/test_1_10/MajorityElement.java
@@ -7,7 +7,27 @@ package ink.lichen.leecode.test.test_1_10;
 public class MajorityElement {
 
     public int majorityElement(int[] nums) {
+        int n = nums.length;
+        if ( n == 1){
+            return nums[0];
+        }
+        int tmp = nums[0];
+        int count = 1;
+        for (int i=1; i < n ; i++){
+            if (tmp == nums[i]){
+                count++;
+            }else {
+                if (count == 1){
+                    tmp = nums[i];
+                }else {
+                    count--;
+                }
+            }
+        }
+        return tmp;
+    }
 
-        return 0;
+    public static void main(String[] args) {
+        System.out.println(new MajorityElement().majorityElement(new int[]{3,2,3}));
     }
 }
---
+++ src/main/java/ink/lichen/leecode/test/test_1_10/SortList.java
@@ -0,0 +1,16 @@
+package ink.lichen.leecode.test.test_1_10;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-10.
+ */
+public class SortList {
+
+
+    public ListNode sortList(ListNode head) {
+
+
+        return null;
+    }
+}
