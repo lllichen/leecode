caaff589d4c4e3ff182065d0e4590abeac59b87a
lichen <lichen@daojia-inc.com>
2019-01-11 18:45:04 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_1_11/LowestCommonAncestor2.java
+++ src/main/java/ink/lichen/leecode/test/test_1_11/LowestCommonAncestor2.java
@@ -9,7 +9,13 @@ public class LowestCommonAncestor2 {
 
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q){
+            return root;
+        }
+         TreeNode leftNode = lowestCommonAncestor(root.left,p,q);
+        TreeNode rightNode = lowestCommonAncestor(root.right,p,q);
+        if (leftNode != null && rightNode != null)return root;
 
-        return null;
+        return leftNode != null ? leftNode : rightNode;
     }
 }
