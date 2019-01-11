b09d8a4caced6be5a357761f6b95be5ca1a4287c
lichen <lichen@daojia-inc.com>
2019-01-11 14:12:52 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_1_11/FindKthLargest.java
+++ src/main/java/ink/lichen/leecode/test/test_1_11/FindKthLargest.java
@@ -20,7 +20,34 @@ public class FindKthLargest {
 
     private void quickSelect(int[] nums, int left, int right, int k) {
         if (left < right){
+            int pivot = median3(nums,left,right);
+            int i = left,j= right-1;
+            for (;;){
+                while (nums[++i]>pivot){};
+                while (nums[--j]<pivot){};
 
+            }
         }
+    }
+
+    private int median3(int[] nums, int left, int right) {
+        int center = (left+right) >>1;
+        if (nums[center]>nums[left]){
+            swap(nums,left,center);
+        }
+        if (nums[right]>nums[left]){
+            swap(nums,left,right);
+        }
+        if (nums[right]>nums[center]){
+            swap(nums,right,center);
+        }
+        swap(nums,center,right-1);
+        return nums[right-1];
+    }
+
+    private int swap(int[] nums,int a, int b){
+        int t = nums[a];
+        nums[a] = nums[b];
+        nums[b] = t;
     }
 }
