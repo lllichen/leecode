4a9e73a9809e94615c85bc73d46bb3c69efccec1
lichen <lichen@daojia-inc.com>
2019-01-11 17:31:01 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_1_11/LowestCommonAncestor2.java
@@ -0,0 +1,15 @@
+package ink.lichen.leecode.test.test_1_11;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-11.
+ */
+public class LowestCommonAncestor2 {
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+}
