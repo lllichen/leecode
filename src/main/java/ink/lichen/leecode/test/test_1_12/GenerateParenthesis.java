9810c06ecb33f0093fc3068179846dc7afba3a68
lichen <lichen@daojia-inc.com>
2019-01-12 10:54:48 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_1_12/GenerateParenthesis.java
@@ -0,0 +1,22 @@
+package ink.lichen.leecode.test.test_1_12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-12.
+ */
+public class GenerateParenthesis {
+
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        help(res);
+        return res;
+    }
+
+    public void help(List<String> res ){
+
+    }
+
+}
