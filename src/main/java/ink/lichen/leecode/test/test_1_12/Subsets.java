cf3e1ba9046dd8e1f5d575540f1dbb51eaf83f49
lichen <lichen@daojia-inc.com>
2019-01-12 16:24:36 +0800

    add subSet

--- src/main/java/ink/lichen/leecode/test/test_1_12/Subsets.java
+++ src/main/java/ink/lichen/leecode/test/test_1_12/Subsets.java
@@ -25,10 +25,16 @@ public class Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(res);
+        backtrack(res,new ArrayList<>(),nums,0);
         return res;
     }
 
-    private void backtrack(List<List<Integer>> res) {
+    private void backtrack(List<List<Integer>> res,List<Integer> list,int[] nums, int start) {
+        res.add(new ArrayList<>(list));
+        for (int i = start; i < nums.length; i++){
+            list.add(nums[i]);
+            backtrack(res,list,nums,i+1);
+            list.remove(list.size() -1);
+        }
     }
 }
