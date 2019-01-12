416ad9f7034b08f923671e375ae45ba2b232966a
lichen <lichen@daojia-inc.com>
2019-01-12 18:00:43 +0800

    add permute

--- src/main/java/ink/lichen/leecode/test/test_1_12/Permute.java
+++ src/main/java/ink/lichen/leecode/test/test_1_12/Permute.java
@@ -20,13 +20,35 @@ import java.util.List;
  */
 public class Permute {
 
+    List<List<Integer>> lists = new ArrayList<>();
+
     public List<List<Integer>> permute(int[] nums) {
+        permute(nums,0);
+        return lists;
 
-        List<List<Integer>> res = new ArrayList<>();
-        backtrack();
-        return res;
     }
+    private void permute(int [] nums,int start){
+        if(start>=nums.length){
+            List<Integer> list = array2list(nums);
+            lists.add(list);
+        }
 
-    private void backtrack() {
+        for(int i = start;i<nums.length;i++){
+            swap(nums,start,i);
+            permute(nums,start+1);
+            swap(nums,start,i);
+        }
+    }
+    private void swap(int [] nums,int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private List<Integer> array2list(int[] array){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < array.length;i++){
+            list.add(array[i]);
+        }
+        return list;
     }
 }
