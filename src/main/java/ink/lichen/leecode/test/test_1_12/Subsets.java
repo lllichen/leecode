3272665705210ecb26b80ddacd0342c03c68111e
lichen <lichen@daojia-inc.com>
2019-01-12 17:51:10 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_1_12/Subsets.java
+++ src/main/java/ink/lichen/leecode/test/test_1_12/Subsets.java
@@ -25,16 +25,20 @@ public class Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(res,new ArrayList<>(),nums,0);
+        backtrack(res,new ArrayList<Integer>(),nums,0);
         return res;
     }
 
-    private void backtrack(List<List<Integer>> res,List<Integer> list,int[] nums, int start) {
+    //1 12 123 1234
+    private void backtrack(List<List<Integer>> res, ArrayList<Integer> list, int[] nums, int start) {
         res.add(new ArrayList<>(list));
-        for (int i = start; i < nums.length; i++){
+        if (start >= nums.length){
+            return;
+        }
+        for (int i = start; i < nums.length;i++ ){
             list.add(nums[i]);
             backtrack(res,list,nums,i+1);
-            list.remove(list.size() -1);
+            list.remove(list.size()-1);
         }
     }
 
