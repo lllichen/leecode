e6c6d27c3da1ee4d0be4c185c01ac9f0d13031c7
lichen <lichen@daojia-inc.com>
2019-01-12 12:16:53 +0800

    add GenerateParenthesis

--- src/main/java/ink/lichen/leecode/test/test_1_12/GenerateParenthesis.java
+++ src/main/java/ink/lichen/leecode/test/test_1_12/GenerateParenthesis.java
@@ -5,21 +5,50 @@ import java.util.List;
 
 /**
  * Created by lichen@daojia.com on 2019-1-12.
+ *
+ * ((( )))
+ * (()())
+ *
  */
 public class GenerateParenthesis {
 
 
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        help(res,"",0,n);
+        backtrack(res,"",0,0,n);
         return res;
     }
 
-    public void help(List<String> res,String str,int i, int n){
-        if (str.length() == 2*n){
-            res.add(str);
+
+    /**
+     *
+     *
+     *
+     *  33
+     *  32
+     *  31
+     *  30
+     *  20
+     *  10
+     *  00
+     *
+     */
+    public void backtrack(List<String> list, String str ,Integer open,Integer close, Integer max)
+    {
+        if (str.length() == max*2){
+            list.add(str);
+            return;
         }
 
+        if (open < max){
+            backtrack(list,str+"(",open+1,close,max);
+        }
+        if (close < open){
+            backtrack(list,str+")",open,close+1,max);
+        }
     }
 
+    public static void main(String[] args) {
+        new GenerateParenthesis().generateParenthesis(3);
+    }
 }
