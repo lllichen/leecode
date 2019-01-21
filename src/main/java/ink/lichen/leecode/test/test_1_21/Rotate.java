6b3031cd9aa2eeba560084d4c4979e0a7608e6dc
lichen <lichen@daojia-inc.com>
2019-01-21 18:39:47 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_1_21/Rotate.java
+++ src/main/java/ink/lichen/leecode/test/test_1_21/Rotate.java
@@ -2,6 +2,29 @@ package ink.lichen.leecode.test.test_1_21;
 
 /**
  * Created by lichen@daojia.com on 2019-1-21.
+ *
+ * 给定 matrix =
+ * [
+ *   [1,2,3],
+ *   [4,5,6],
+ *   [7,8,9]
+ * ],
+ *
+ *
+ * [
+ *   [9,6,3],
+ *   [8,5,2],
+ *   [7,4,1]
+ * ],
+ *
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ *   [7,4,1],
+ *   [8,5,2],
+ *   [9,6,3]
+ * ]
+ *
  */
 public class Rotate {
 
