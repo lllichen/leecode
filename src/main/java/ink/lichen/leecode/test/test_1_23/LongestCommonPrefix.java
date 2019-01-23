e1b64eff15440a786174efd019c70a685f60f565
lichen <lichen@daojia-inc.com>
2019-01-23 16:54:47 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_1_23/LongestCommonPrefix.java
+++ src/main/java/ink/lichen/leecode/test/test_1_23/LongestCommonPrefix.java
@@ -7,7 +7,25 @@ public class LongestCommonPrefix {
 
 
     public String longestCommonPrefix(String[] strs) {
-
-        return "";
+        int n = strs.length;
+        if (n == 0){
+            return "";
+        }
+        int min = strs[0].length();
+        for (String str : strs){
+            if (min > str.length()){
+                min = str.length();
+            }
+        }
+        int i;
+        for (i = 0; i < min; i++){
+            char t = strs[0].charAt(i);
+            for (int j = 1; j < n; j++){
+                if (t != strs[j].charAt(i)){
+                    return strs[0].substring(0,i);
+                }
+            }
+        }
+        return strs[0].substring(0,i);
     }
 }
