5f3c59d47fbab40cfdd5189bc2532c58bf2718ab
lichen <lichen@daojia-inc.com>
2019-01-23 17:04:02 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_1_23/DeleteNode.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.test.test_1_23;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-23.
+ */
+public class DeleteNode {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
