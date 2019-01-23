ee6d14175c6e6ed5605a45d2c106a1bd5a424a5d
lichen <lichen@daojia-inc.com>
2019-01-23 18:07:49 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_1_23/ReverseList.java
@@ -0,0 +1,16 @@
+package ink.lichen.leecode.test.test_1_23;
+
+import ink.lichen.leecode.support.ListNode;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-23.
+ */
+public class ReverseList {
+
+
+    public ListNode reverseList(ListNode head) {
+
+        return null;
+    }
+
+}
