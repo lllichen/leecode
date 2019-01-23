778841c08a2ca2d906a37a431cd8ffb200445b5a
lichen <lichen@daojia-inc.com>
2019-01-23 16:40:09 +0800

    add CountAndSay

--- src/main/java/ink/lichen/leecode/test/test_1_23/CountAndSay.java
+++ src/main/java/ink/lichen/leecode/test/test_1_23/CountAndSay.java
@@ -2,6 +2,17 @@ package ink.lichen.leecode.test.test_1_23;
 
 /**
  * Created by lichen@daojia.com on 2019-1-23.
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
  */
 public class CountAndSay {
+
+
+    public String countAndSay(int n) {
+
+        return "";
+    }
 }
