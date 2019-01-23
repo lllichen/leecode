9b1b74cdc845e68db4231c45aef2355fafd44f08
lichen <lichen@daojia-inc.com>
2019-01-23 16:46:24 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_1_23/LongestCommonPrefix.java
@@ -0,0 +1,13 @@
+package ink.lichen.leecode.test.test_1_23;
+
+/**
+ * Created by lichen@daojia.com on 2019-1-23.
+ */
+public class LongestCommonPrefix {
+
+
+    public String longestCommonPrefix(String[] strs) {
+
+        return "";
+    }
+}
