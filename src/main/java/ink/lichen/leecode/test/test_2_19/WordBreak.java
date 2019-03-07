842624defd276ee84b637e94dd3e6b4925d9cd61
lichen <lichen@daojia-inc.com>
2019-03-07 15:46:53 +0800

    add wordBreak

---
+++ src/main/java/ink/lichen/leecode/test/test_2_19/WordBreak.java
@@ -0,0 +1,37 @@
+package ink.lichen.leecode.test.test_2_19;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * Created by lichen@daojia.com on 2019-3-7.
+ */
+public class WordBreak {
+
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length()+1];
+        dp[0] = true;
+        for (int i = 1 ; i < s.length();i++){
+            for (int j = i;j >=0; j--){
+                String substring = s.substring(j,i+1);
+                if (set.contains(substring) && dp[j]){
+                    dp[j] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        WordBreak wordBreak = new WordBreak();
+        wordBreak.wordBreak("leetcode", Arrays.asList("leet","code"));
+    }
+}
