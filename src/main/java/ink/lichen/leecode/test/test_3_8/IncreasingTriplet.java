4f511efcdec2675bf9a9b4c584afe5ba4092a6a7
lichen <lichen@daojia-inc.com>
2019-03-08 18:44:22 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_3_8/IncreasingTriplet.java
+++ src/main/java/ink/lichen/leecode/test/test_3_8/IncreasingTriplet.java
@@ -2,12 +2,35 @@ package ink.lichen.leecode.test.test_3_8;
 
 /**
  * Created by lichen@daojia.com on 2019-3-8.
+ *给定一个未排序的数组，判断这个数组中是否存在长度为 3 的递增子序列。
+ * 输入: [1,2,3,4,5]
+ * 输出: true
  */
 public class IncreasingTriplet {
 
 
     public boolean increasingTriplet(int[] nums) {
+        if (nums.length< 3){
+            return false;
+        }
 
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums){
+            if (first> num){
+                first = num;
+            }else if ( num > first && num < second ){
+                second = num;
+            }else if (num > second){
+                return true;
+            }
+        }
         return false;
+    }
+
+    public static void main(String[] args) {
+        IncreasingTriplet increasingTriplet = new IncreasingTriplet();
+        increasingTriplet.increasingTriplet(new int[]{1,2,3,4,5});
     }
 }
---
+++ src/main/java/ink/lichen/leecode/test/test_3_8/ProductExceptSelf.java
@@ -0,0 +1,35 @@
+package ink.lichen.leecode.test.test_3_8;
+
+/**
+ * Created by lichen@daojia.com on 2019-3-8.
+ * 输入: [1,2,3,4]
+ *       [1,2,6,12]
+ * 输出: [24,12,8,6]
+ */
+public class ProductExceptSelf {
+
+
+    public int[] productExceptSelf(int[] nums) {
+        if(nums.length <= 1){
+            return nums;
+        }
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        int left = 1, right = 1;
+        // 计算每个点左边的乘积
+        for(int i = 1; i < nums.length; i++){
+            left = left * nums[i - 1];
+            res[i] = left;
+        }
+        // 计算每个点右边的乘积
+        for(int i = nums.length - 2; i >= 0; i--){
+            right = right * nums[i + 1];
+            res[i] = right * res[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new ProductExceptSelf().productExceptSelf(new int[]{1,2,3,4});
+    }
+}
