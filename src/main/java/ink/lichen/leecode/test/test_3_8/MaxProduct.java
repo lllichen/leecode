7e7cfa975da841f15693a222476295930f349c8a
lichen <lichen@daojia-inc.com>
2019-03-08 11:35:12 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_3_8/MaxProduct.java
+++ src/main/java/ink/lichen/leecode/test/test_3_8/MaxProduct.java
@@ -2,9 +2,10 @@ package ink.lichen.leecode.test.test_3_8;
 
 /**
  * Created by lichen@daojia.com on 2019-3-8.
- * 输入: [2,3,-2,4]
+ * 输入: [2,3,-2,-4]
  * 输出: 6
  * 解释: 子数组 [2,3] 有最大乘积 6。
+ *
  */
 public class MaxProduct {
 
@@ -13,16 +14,22 @@ public class MaxProduct {
         if (n == 0){
             return 0;
         }
-        int [] right = new int[n+1];
-        int [] neg = new int[n+1];
-        right[0] = 1;neg[0] = 1;
-        for (int i = 0; i < n ; i++){
-            int val = right[i]*nums[i];
-
+        int max =nums[0];
+        int [] right = new int[n];
+        int [] neg = new int[n];
+        right[0] = nums[0];
+        neg[0] = nums[0];
+        for (int i = 1; i < n ; i++){
+            right[i] = Math.max(Math.max(right[i-1]*nums[i],neg[i-1]*nums[i]),nums[i]);
+            neg[i] = Math.min(Math.min(right[i-1]*nums[i],neg[i-1]*nums[i]),nums[i]);
+            max = Math.max(max,right[i]);
         }
+        return max;
+    }
 
-
-        return 0;
+    public static void main(String[] args) {
+        MaxProduct maxProduct = new MaxProduct();
+        System.out.println(maxProduct.maxProduct(new int[]{2,3,-2,-4}));
     }
 
 }
