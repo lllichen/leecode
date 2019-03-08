4fe4f5138cebbcf9e022eb470af7d259a9ceec6b
lichen <lichen@daojia-inc.com>
2019-03-08 17:16:32 +0800

    add Rotate

---
+++ src/main/java/ink/lichen/leecode/test/test_3_8/Rotate.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.test.test_3_8;
+
+/**
+ * Created by lichen@daojia.com on 2019-3-8.
+ */
+public class Rotate {
+
+
+    public void rotate(int[] nums, int k) {
+
+    }
+
+
+}
---
+++ src/main/java/ink/lichen/leecode/test/test_3_8/Solution.java
@@ -0,0 +1,44 @@
+package ink.lichen.leecode.test.test_3_8;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by lichen@daojia.com on 2019-3-8.
+ */
+public class Solution {
+
+    private int[] origin;
+    private int[] result;
+
+    public Solution(int[] nums) {
+        this.origin = nums;
+        this.result = Arrays.copyOf(nums, nums.length);
+    }
+
+    public int[] reset() {
+        return origin;
+    }
+
+    private Random random = new Random();
+    /**
+     * 交换数组，数组长度随机
+     */
+    public int[] shuffle() {
+        for (int i = 0; i < result.length; i++) {
+            int idx = random.nextInt(result.length);
+            swap(i,idx);
+        }
+        return result;
+    }
+
+    private void swap(int i, int j){
+        int tem = result[i];
+        result[i] = result[j];
+        result[j] = tem;
+    }
+
+    public static void main(String[] args) {
+        Solution  solution = new Solution(new int[]{1,2,3});
+    }
+}
