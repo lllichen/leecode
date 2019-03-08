9c307c099cbca50980ddf2892b676bef46e205eb
lichen <lichen@daojia-inc.com>
2019-03-08 10:22:02 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_3_8/MaxProduct.java
+++ src/main/java/ink/lichen/leecode/test/test_3_8/MaxProduct.java
@@ -2,10 +2,18 @@ package ink.lichen.leecode.test.test_3_8;
 
 /**
  * Created by lichen@daojia.com on 2019-3-8.
+ * 输入: [2,3,-2,4]
+ * 输出: 6
+ * 解释: 子数组 [2,3] 有最大乘积 6。
  */
 public class MaxProduct {
 
     public int maxProduct(int[] nums) {
+        int n = nums.length;
+
+        if (n == 0){
+            return 0;
+        }
 
         return 0;
     }
