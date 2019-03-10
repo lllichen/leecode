660d5259dfa5f9deb425215e13142c5516498e69
lichen <togetherlichen@gmail.com>
2019-03-10 10:39:45 +0800

    add all info

---
+++ src/main/java/ink/lichen/leecode/test/test_2_19/IsPalindrome.java
@@ -0,0 +1,14 @@
+package ink.lichen.leecode.test.test_2_19;
+
+import java.util.LinkedHashSet;
+
+public class IsPalindrome {
+
+
+
+
+
+    public static void main(String[] args) {
+        Integer.
+    }
+}
