0fe923f1dcef3d6e598b9110ada8bfc1d83b8ed2
lichen <lichen@daojia-inc.com>
2019-03-11 16:24:37 +0800

    add info

---
+++ src/main/java/ink/lichen/leecode/test/test_3_11/kthSmallest.java
@@ -0,0 +1,16 @@
+package ink.lichen.leecode.test.test_3_11;
+
+import ink.lichen.leecode.support.TreeNode;
+
+/**
+ * Created by lichen@daojia.com on 2019-3-11.
+ */
+public class kthSmallest {
+
+
+    public int kthSmallest(TreeNode root, int k) {
+
+        return 0;
+    }
+
+}
