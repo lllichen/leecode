5eb431750e909f8baff014b56909078f86e46ff8
lichen <lichen@daojia-inc.com>
2019-03-11 15:30:26 +0800

    add PostorderTraversal

---
+++ src/main/java/ink/lichen/leecode/test/test_3_11/PostorderTraversal.java
@@ -0,0 +1,43 @@
+package ink.lichen.leecode.test.test_3_11;
+
+import ink.lichen.leecode.support.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by lichen@daojia.com on 2019-3-11.
+ */
+public class PostorderTraversal {
+
+//    public List<Integer> postorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//        help(result,root);
+//        return result;
+//    }
+//
+//    private void help(List<Integer> result,TreeNode root){
+//        if (root != null){
+//            help(result,root.left);
+//            help(result,root.right);
+//            result.add(root.val);
+//        }
+//    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        if (root == null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode left = stack.pop();
+
+//            result.add(stack.)
+        }
+        return result;
+    }
+}
