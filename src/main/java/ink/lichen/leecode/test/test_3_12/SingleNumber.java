c454af2706075ed2722672ec766ea55fca75cdbd
lichen <lichen@daojia-inc.com>
2019-03-12 18:11:54 +0800

    add info

--- src/main/java/ink/lichen/leecode/test/test_3_12/SingleNumber.java
+++ src/main/java/ink/lichen/leecode/test/test_3_12/SingleNumber.java
@@ -2,6 +2,21 @@ package ink.lichen.leecode.test.test_3_12;
 
 /**
  * Created by lichen@daojia.com on 2019-3-12.
+ * 输入: [0,1,0,1,0,1,99]
+ * 输出: 99
  */
 public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int a=0,b=0;
+        for (int c : nums) {
+            b = b ^ c & ~ a;
+            a = a ^ c & ~ b;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
