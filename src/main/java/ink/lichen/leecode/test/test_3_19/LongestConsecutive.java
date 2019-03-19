4b68fb5d3aae33bd64c4a781e90ac57d546c854e
lichen <lichen@daojia-inc.com>
2019-03-19 15:20:25 +0800

    add LongestConsecutive

---
+++ src/main/java/ink/lichen/leecode/test/test_3_19/LongestConsecutive.java
@@ -0,0 +1,12 @@
+package ink.lichen.leecode.test.test_3_19;
+
+/**
+ * Created by lichen@daojia.com on 2019-3-19.
+ */
+public class LongestConsecutive {
+
+    public int longestConsecutive(int[] nums) {
+
+        return 0;
+    }
+}
