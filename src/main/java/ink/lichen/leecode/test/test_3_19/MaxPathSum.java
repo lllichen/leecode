8016cdebd629bc5235dd80621c3707e3c6f68890
lichen <lichen@daojia-inc.com>
2019-03-19 15:16:25 +0800

    add ifo

--- src/main/java/ink/lichen/leecode/test/test_3_19/MaxPathSum.java
+++ src/main/java/ink/lichen/leecode/test/test_3_19/MaxPathSum.java
@@ -15,13 +15,30 @@ import ink.lichen.leecode.support.TreeNode;
  *
  * 输出: 42
  *
+ *
  */
 public class MaxPathSum {
 
+    int maxValue;
 
     public int maxPathSum(TreeNode root) {
+        maxValue = root.val;
+        maxPathDown(root);
+        return maxValue;
+    }
+    private int maxPathDown(TreeNode node){
+        if(node==null)
+            return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+        maxValue = Math.max(maxValue, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
 
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(2);
+        node.left = new TreeNode(-1);
 
-        return 0;
+        System.out.println(new MaxPathSum().maxPathSum(node));
     }
 }
